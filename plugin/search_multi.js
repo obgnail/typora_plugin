729d70f360afb355a3dcd2c185e96e1367e75791
obgnail <he1251698542@gmail.com>
2023-06-20 13:20:47 +0800

    rollback

--- plugin/search_multi.js
+++ plugin/search_multi.js
@@ -1,138 +1,140 @@
 window.onload = () => {
     const config = {
         caseSensitive: false,
-        filetypes: ["", "md", "markdown", "mdown", "mmd", "text", "txt", "rmarkdown", "mkd", "mdwn", "mdtxt", "rmd", "mdtext", "apib"],
-        hotkey: (event) => event.ctrlKey && event.shiftKey && event.keyCode === 80, // 懒得写keycode映射函数,能用就行
+        separator: " ",
+        filetypes: ["", "md", "markdown", "mdown", "mmd", "text", "txt", "rmarkdown",
+            "mkd", "mdwn", "mdtxt", "rmd", "mdtext", "apib"],
+        hotkey: event => event.ctrlKey && event.shiftKey && event.keyCode === 80, // 懒得写keycode映射函数,能用就行
     };
 
     (() => {
         // insert css
         const modal_css = `
-        #typora-search-multi {
-            position: fixed;
-            left: 50%;
-            width: 420px;
-            margin-left: -200px;
-            z-index: 9999;
-            padding: 4px;
-            background-color: #f8f8f8;
-            box-shadow: 0 4px 10px rgba(0, 0, 0, .5);
-            border: 1px solid #ddd;
-            border-top: none;
-            color: var(--text-color);
-            margin-top: 0;
-            transform: translate3d(0, 0, 0)
-        }
-        
-        #typora-search-multi .ty-quick-open-category-title {
-            border-top: none;
-        }
-        
-        .mac-seamless-mode #typora-search-multi {
-            top: 30px
-        }
-        
-        .mac-seamless-mode .modal-dialog {
-            margin-top: 40px
-        }
-        
-        #typora-search-multi-input {
-            position: relative;
-        }
-        
-        #typora-search-multi-input input {
-            width: 100%;
-            font-size: 14px;
-            line-height: 25px;
-            max-height: 27px;
-            overflow: auto;
-            border: 1px solid #ddd;
-            box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
-            border-radius: 2px;
-            padding-left: 5px
-        }
-        
-        #typora-search-multi-input input:focus {
-            outline: 0
-        }
-        
-        #typora-search-multi-input svg.icon {
-            width: 20px;
-            height: 14px;
-            stroke: none;
-            fill: currentColor
-        }
-        
-        #typora-search-multi-input .searchpanel-search-option-btn {
-            position: absolute;
-            right: 6px;
-            top: 6px;
-            opacity: .5;
-            border: none
-        }
-        
-        #typora-search-multi-input .searchpanel-search-option-btn.select,
-        #typora-search-multi-input .searchpanel-search-option-btn:hover {
-            background: var(--active-file-bg-color);
-            color: var(--active-file-text-color);
-            opacity: 1
-        }
-        
-        .typora-search-multi-item {
-            display: block;
-            font-size: 14px;
-            height: 40px;
-            padding-left: 20px;
-            padding-right: 20px;
-            padding-top: 2px;
-            overflow: hidden
-        }
-        
-        .typora-search-multi-item:hover,
-        .typora-search-multi-item.active {
-            background-color: var(--active-file-bg-color);
-            border-color: var(--active-file-text-color);
-            color: var(--active-file-text-color);
-            cursor: pointer;
-        }
-        
-        .typora-search-multi-item-title {
-            line-height: 24px;
-            max-height: 24px;
-            overflow: hidden
-        }
-        
-        .typora-search-multi-list {
-            margin-top: 0;
-            cursor: default;
-            max-height: 320px;
-            overflow-x: hidden;
-            overflow-y: auto;
-        }
-        
-        .typora-search-multi-list-inner {
-            position: relative
-        }
-        
-        
-        .typora-search-multi-item-path {
-            opacity: .5;
-            font-size: 11px;
-            margin-top: -4px;
-            text-overflow: ellipsis;
-            width: 100%;
-            overflow: hidden;
-            white-space: nowrap;
-            line-height: 14px
-        }
-        
-        .typora-search-multi-info-item {
-            opacity: .7;
-            font-size: 12px;
-            line-height: 40px;
-            position: relative;
-            padding-left: 20px
-        }`
+            #typora-search-multi {
+                position: fixed;
+                left: 50%;
+                width: 420px;
+                margin-left: -200px;
+                z-index: 9999;
+                padding: 4px;
+                background-color: #f8f8f8;
+                box-shadow: 0 4px 10px rgba(0, 0, 0, .5);
+                border: 1px solid #ddd;
+                border-top: none;
+                color: var(--text-color);
+                margin-top: 0;
+                transform: translate3d(0, 0, 0)
+            }
+            
+            #typora-search-multi .ty-quick-open-category-title {
+                border-top: none;
+            }
+            
+            .mac-seamless-mode #typora-search-multi {
+                top: 30px
+            }
+            
+            .mac-seamless-mode .modal-dialog {
+                margin-top: 40px
+            }
+            
+            #typora-search-multi-input {
+                position: relative;
+            }
+            
+            #typora-search-multi-input input {
+                width: 100%;
+                font-size: 14px;
+                line-height: 25px;
+                max-height: 27px;
+                overflow: auto;
+                border: 1px solid #ddd;
+                box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+                border-radius: 2px;
+                padding-left: 5px
+            }
+            
+            #typora-search-multi-input input:focus {
+                outline: 0
+            }
+            
+            #typora-search-multi-input svg.icon {
+                width: 20px;
+                height: 14px;
+                stroke: none;
+                fill: currentColor
+            }
+            
+            #typora-search-multi-input .searchpanel-search-option-btn {
+                position: absolute;
+                right: 6px;
+                top: 6px;
+                opacity: .5;
+                border: none
+            }
+            
+            #typora-search-multi-input .searchpanel-search-option-btn.select,
+            #typora-search-multi-input .searchpanel-search-option-btn:hover {
+                background: var(--active-file-bg-color);
+                color: var(--active-file-text-color);
+                opacity: 1
+            }
+            
+            .typora-search-multi-item {
+                display: block;
+                font-size: 14px;
+                height: 40px;
+                padding-left: 20px;
+                padding-right: 20px;
+                padding-top: 2px;
+                overflow: hidden
+            }
+            
+            .typora-search-multi-item:hover,
+            .typora-search-multi-item.active {
+                background-color: var(--active-file-bg-color);
+                border-color: var(--active-file-text-color);
+                color: var(--active-file-text-color);
+                cursor: pointer;
+            }
+            
+            .typora-search-multi-item-title {
+                line-height: 24px;
+                max-height: 24px;
+                overflow: hidden
+            }
+            
+            .typora-search-multi-list {
+                margin-top: 0;
+                cursor: default;
+                max-height: 320px;
+                overflow-x: hidden;
+                overflow-y: auto;
+            }
+            
+            .typora-search-multi-list-inner {
+                position: relative
+            }
+            
+            
+            .typora-search-multi-item-path {
+                opacity: .5;
+                font-size: 11px;
+                margin-top: -4px;
+                text-overflow: ellipsis;
+                width: 100%;
+                overflow: hidden;
+                white-space: nowrap;
+                line-height: 14px
+            }
+            
+            .typora-search-multi-info-item {
+                opacity: .7;
+                font-size: 12px;
+                line-height: 40px;
+                position: relative;
+                padding-left: 20px
+            }`
         const style = document.createElement('style');
         style.type = 'text/css';
         style.innerHTML = modal_css;
@@ -140,40 +142,40 @@ window.onload = () => {
 
         // insert html
         const search_div = `
-    <div id="typora-search-multi-input">
-        <input type="text" class="input" tabindex="1" autocorrect="off" spellcheck="false"
-            autocapitalize="off" value="" placeholder="多关键字查找"
-            data-localize="Search by file name" data-lg="Front">
-        <span ty-hint="区分大小写" id="typora-search-multi-case-option-btn" class="searchpanel-search-option-btn" aria-label="区分大小写">
-            <svg class="icon">
-                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#find-and-replace-icon-case"></use>
-            </svg>
-        </span>
-    </div>
-
-    <div class="typora-search-multi-list" id="typora-search-multi-list" style="display:none">
-        <div class="ty-quick-open-category ty-has-prev" id="ty-quick-open-infolder-category">
-            <div class="ty-quick-open-category-title" data-localize="File Results" data-lg="Menu" style="height: auto;">
-                匹配的文件
+            <div id="typora-search-multi-input">
+                <input type="text" class="input" tabindex="1" autocorrect="off" spellcheck="false"
+                    autocapitalize="off" value="" placeholder="多关键字查找，空格为分隔符"
+                    data-localize="Search by file name" data-lg="Front">
+                <span ty-hint="区分大小写" id="typora-search-multi-case-option-btn" class="searchpanel-search-option-btn" aria-label="区分大小写">
+                    <svg class="icon">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#find-and-replace-icon-case"></use>
+                    </svg>
+                </span>
             </div>
-            <div class="typora-search-multi-list-inner" style="height: 520px;">
-                <div class="quick-open-group-block" data-block-index="0"
-                    style="position: absolute; top: 0; width: 100%;">
+        
+            <div class="typora-search-multi-list" id="typora-search-multi-list" style="display:none">
+                <div class="ty-quick-open-category ty-has-prev" id="ty-quick-open-infolder-category">
+                    <div class="ty-quick-open-category-title" data-localize="File Results" data-lg="Menu" style="height: auto;">
+                        匹配的文件
+                    </div>
+                    <div class="typora-search-multi-list-inner" style="height: 520px;">
+                        <div class="quick-open-group-block" data-block-index="0"
+                            style="position: absolute; top: 0; width: 100%;">
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-
-    <div class="typora-search-multi-info-item" style="display:none">
-        <div class="typora-search-multi-info" data-localize="Searching" data-lg="Front">Searching</div>
-        <div class="typora-search-spinner">
-            <div class="rect1"></div>
-            <div class="rect2"></div>
-            <div class="rect3"></div>
-            <div class="rect4"></div>
-            <div class="rect5"></div>
-        </div>
-    </div>`;
+        
+            <div class="typora-search-multi-info-item" style="display:none">
+                <div class="typora-search-multi-info" data-localize="Searching" data-lg="Front">Searching</div>
+                <div class="typora-search-spinner">
+                    <div class="rect1"></div>
+                    <div class="rect2"></div>
+                    <div class="rect3"></div>
+                    <div class="rect4"></div>
+                    <div class="rect5"></div>
+                </div>
+            </div>`;
         const searchModal = document.createElement("div");
         searchModal.id = 'typora-search-multi';
         searchModal.className = 'modal-dialog';
@@ -308,7 +310,7 @@ window.onload = () => {
         if (!rootPath) {
             return
         }
-        let keyArr = keys.split(" ").filter(Boolean);
+        let keyArr = keys.split(config.separator).filter(Boolean);
         if (!keyArr) {
             return
         }
@@ -333,7 +335,9 @@ window.onload = () => {
         }
     });
 
-    modal.searchBlock.addEventListener("click", (event) => {
+    modal.searchBlock.addEventListener("click", event => {
+        event.preventDefault();
+        event.stopPropagation();
         for (const ele of event.path) {
             if (ele.className === "typora-search-multi-item") {
                 const filepath = ele.getAttribute("data-path");
@@ -343,10 +347,12 @@ window.onload = () => {
         }
     });
 
-    window.onkeydown = (event) => {
+    window.onkeydown = event => {
         if (config.hotkey(event)) {
             modal.searchModal.style.display = "block";
             modal.searchInput.select();
+            event.preventDefault();
+            event.stopPropagation();
         }
     }
 
