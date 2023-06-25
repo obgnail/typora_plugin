a404b2a7e01d753020b6948ab7be5c9989a1d13a
obgnail <he1251698542@gmail.com>
2023-06-26 01:31:05 +0800

    update resize function

--- plugin/resize_table.js
+++ plugin/resize_table.js
@@ -4,6 +4,10 @@
         threshold: 10,
     }
 
+    if (!config.ALLOW_DRAG) {
+        return
+    }
+
     const pxToInt = px => parseInt(px.trim().replace("px", ""))
 
     const whichChildOfParent = child => {
@@ -28,74 +32,67 @@
         return max
     }
 
+    document.querySelector("#write").addEventListener("mousedown", ev => {
+        const target = ev.target.closest("td");
+        if (!target) {
+            return
+        }
 
-    if (config.ALLOW_DRAG) {
-        const write = document.querySelector("#write");
+        let direction = "";
+        const rect = target.getBoundingClientRect();
+        const shiftX = rect.width - ev.clientX;
+        const shiftY = rect.height - ev.clientY;
 
-        write.addEventListener("mousedown", ev => {
-            const target = ev.target.closest("td");
-            if (!target) {
-                return
-            }
-
-            let direction = "";
-            const rect = target.getBoundingClientRect();
-            const startWidth = rect.width;
-            const startHeight = rect.height;
-            const startX = ev.clientX;
-            const startY = ev.clientY;
-
-            const setTds = (tds, attr, value) => {
-                if (value) {
-                    target.style[attr] = value + "px";
-                    for (const td in tds) {
-                        if (td && td.style && td !== target) {
-                            td.style[attr] = "";
-                        }
+        const setTds = (tds, attr, value) => {
+            if (value) {
+                target.style[attr] = value + "px";
+                for (const td in tds) {
+                    if (td && td.style && td !== target) {
+                        td.style[attr] = "";
                     }
                 }
             }
+        }
 
-            if ((ev.clientX > rect.right - config.threshold) || (ev.clientX < rect.left + config.threshold)) {
-                direction = "horizontal";
-                target.style.cursor = "w-resize"
-                const num = whichChildOfParent(target);
-                const tds = target.closest("tbody").querySelectorAll(`tr td:nth-child(${num})`);
-                const width = getMaxPx(tds, "width");
-                setTds(tds, "width", width);
-            } else if ((ev.clientY > rect.bottom - config.threshold) || ev.clientY < rect.top + config.threshold) {
-                direction = "vertical";
-                target.style.cursor = "s-resize"
-                const height = getMaxPx(target.parentElement.children, "height");
-                setTds(target.parentElement.children, "height", height);
-            } else {
-                return
-            }
+        if ((ev.clientX > rect.right - config.threshold) || (ev.clientX < rect.left + config.threshold)) {
+            direction = "horizontal";
+            target.style.cursor = "w-resize"
+            const num = whichChildOfParent(target);
+            const tds = target.closest("tbody").querySelectorAll(`tr td:nth-child(${num})`);
+            const width = getMaxPx(tds, "width");
+            setTds(tds, "width", width);
+        } else if ((ev.clientY > rect.bottom - config.threshold) || ev.clientY < rect.top + config.threshold) {
+            direction = "vertical";
+            target.style.cursor = "s-resize"
+            const height = getMaxPx(target.parentElement.children, "height");
+            setTds(target.parentElement.children, "height", height);
+        } else {
+            return
+        }
 
-            const onMouseMove = ev => {
+        const onMouseMove = ev => {
+            ev.stopPropagation();
+            ev.preventDefault();
+            requestAnimationFrame(() => {
+                if (direction === "vertical") {
+                    target.style.height = ev.clientY + shiftY + "px";
+                } else {
+                    target.style.width = ev.clientX + shiftX + "px";
+                }
+            });
+        }
+
+        document.addEventListener("mouseup", ev => {
                 ev.stopPropagation();
                 ev.preventDefault();
-                requestAnimationFrame(() => {
-                    if (direction === "vertical") {
-                        target.style.height = startHeight + ev.clientY - startY + "px";
-                    } else {
-                        target.style.width = startWidth + ev.clientX - startX + "px";
-                    }
-                });
+                target.style.cursor = "default";
+                document.removeEventListener('mousemove', onMouseMove);
+                target.onmouseup = null;
             }
+        )
 
-            document.addEventListener("mouseup", ev => {
-                    ev.stopPropagation();
-                    ev.preventDefault();
-                    document.removeEventListener('mousemove', onMouseMove);
-                    target.onmouseup = null;
-                    target.style.cursor = "default";
-                }
-            )
-
-            document.addEventListener('mousemove', onMouseMove);
-        })
-    }
+        document.addEventListener('mousemove', onMouseMove);
+    })
 
     console.log("resize_table.js had been injected");
 })()
