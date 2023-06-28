5eec60fe7fd36bec31403881506d50f999d5f6f7
obgnail <he1251698542@gmail.com>
2023-06-28 13:04:52 +0800

    修复 read obly 已知bug

--- plugin/read_only.js
+++ plugin/read_only.js
@@ -6,6 +6,48 @@
         HOTKEY: ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "R",
         // 是否默认使用只读模式
         READ_ONLY_DEFAULT: false,
+        // 只读模式下仍可以使用的快捷键
+        EXCLUDE_KEY: [
+            // 文件
+            ev => metaKeyPressed(ev) && ev.key === "n", // 新建
+            ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "N", // 新建窗口
+            ev => metaKeyPressed(ev) && ev.key === "o", // 打开
+            ev => metaKeyPressed(ev) && ev.key === "p", // 快速打开
+            ev => metaKeyPressed(ev) && ev.key === "s", // 保存
+            ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "S", // 另存
+            ev => metaKeyPressed(ev) && ev.key === ",", // 偏好设置
+            ev => metaKeyPressed(ev) && ev.key === "w", // 关闭
+
+            // 编辑
+            ev => metaKeyPressed(ev) && ev.key === "c", // 复制
+            ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "C", // 复制为markdown
+            ev => metaKeyPressed(ev) && ev.key === "a", // 全选
+            ev => metaKeyPressed(ev) && ev.key === "l", // 选中当前行
+            ev => metaKeyPressed(ev) && ev.key === "e", // 选中当前格式文本
+            ev => metaKeyPressed(ev) && ev.key === "d", // 选中当前词
+            ev => metaKeyPressed(ev) && ev.key === "j", // 跳转到所选内容
+            ev => metaKeyPressed(ev) && ev.key === "Home", // 跳转到文首
+            ev => metaKeyPressed(ev) && ev.key === "End", // 跳转到文末
+            ev => metaKeyPressed(ev) && ev.key === "f", // 查找
+            ev => ev.key === "F3", // 查找下一个
+            ev => ev.shiftKey && ev.key === "F3", // 查找上一个
+
+            // 视图
+            ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "L", // 显示/隐藏侧边栏
+            ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "!", // 大纲
+            ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "@", // 文档列表
+            ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "#", // 文档树
+            ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "F", // 搜索
+            ev => metaKeyPressed(ev) && ev.key === "/", // 源码模式
+            ev => ev.key === "F8", // 专注模式
+            ev => ev.key === "F9", // 打字机模式
+            ev => ev.key === "F11", // 全屏
+            ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "(",  // 实际大小
+            ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "+",  // 放大
+            ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "_",  // 缩小
+            ev => metaKeyPressed(ev) && ev.key === "Tab", // 应用内窗口切换
+            ev => ev.shiftKey && ev.key === "F12", // 开发者工具
+        ],
 
         // 脚本内部使用
         DEBUG: false,
@@ -45,6 +87,15 @@
 
     const metaKeyPressed = ev => File.isMac ? ev.metaKey : ev.ctrlKey;
 
+    const isExclude = ev => {
+        for (const func of config.EXCLUDE_KEY) {
+            if (func(ev)) {
+                return true
+            }
+        }
+        return false
+    }
+
     const write = document.getElementById("write");
     const setEdit = contenteditable => {
         let eleList = write.querySelectorAll(`[contenteditable="${!contenteditable}"]`);
@@ -67,11 +118,20 @@
         if (config.HOTKEY(ev)) {
             ev.preventDefault();
             ev.stopPropagation();
-            document.activeElement.blur();
 
             config.READ_ONLY = !config.READ_ONLY;
             setContentEditable();
             showNotification();
+            document.activeElement.blur();
+            return
+        }
+
+        if (config.READ_ONLY) {
+            if ((ev.key === "Enter") || (ev.target.closest(".md-fences") && !isExclude(ev))) {
+                document.activeElement.blur();
+                ev.preventDefault();
+                ev.stopPropagation();
+            }
         }
     }, true)
 
