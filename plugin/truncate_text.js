04cf222b3df4ff12378b6f2909dc54387f36c1ef
obgnail <he1251698542@gmail.com>
2023-07-01 00:13:59 +0800

    truncate text 提高默认显示的文本段数量，并允许多次操作

--- plugin/truncate_text.js
+++ plugin/truncate_text.js
@@ -1,11 +1,13 @@
 (() => {
     const config = {
+        // 使用启用只读模式脚本,若为false,以下配置全部失效
         ENABLE: true,
+        // 快捷键
         HOTKEY: ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "B",
-        TRUNCATE_LENGTH: 30,
+        // 剩余文本段
+        TRUNCATE_LENGTH: 80,
 
         DEBUG: false,
-        ONCE: true,
     }
 
     if (!config.ENABLE) {
@@ -15,11 +17,10 @@
     const metaKeyPressed = ev => File.isMac ? ev.metaKey : ev.ctrlKey;
 
     window.addEventListener("keydown", ev => {
-        if (config.ONCE && config.HOTKEY(ev)) {
+        if (config.HOTKEY(ev)) {
             ev.preventDefault();
             ev.stopPropagation();
 
-            config.ONCE = false;
             const write = document.getElementById("write");
             const length = write.children.length;
             if (length > config.TRUNCATE_LENGTH) {
