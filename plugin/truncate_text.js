8f59120ccdfaab7f73847ccf80957f9269635bc8
QHQIII <49380606+QHQIII@users.noreply.github.com>
2023-07-01 16:07:54 +0800

    Update truncate_text.js
    <kbd>ctrl+shift+u</kbd>:重新显示所有文本
    <kbd>ctrl+shift+y</kbd>:显示当前可视范围上下40段文本

--- plugin/truncate_text.js
+++ plugin/truncate_text.js
@@ -4,6 +4,9 @@
         ENABLE: true,
         // 快捷键
         HOTKEY: ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "B",
+        HOTKEY_SHOWALL: ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "U",
+        HOTKEY_ATVIEW: ev => metaKeyPressed(ev) && ev.shiftKey && ev.key === "Y",
+
         // 剩余文本段
         TRUNCATE_LENGTH: 80,
 
@@ -31,9 +34,51 @@
         }
     }, true)
 
+    window.addEventListener("keydown", ev => {
+        if (config.HOTKEY_SHOWALL(ev)) {
+            ev.preventDefault();
+            ev.stopPropagation();
+
+            document.getElementById("write").children.forEach(element => {
+                element.style = "";
+            });
+
+        }
+    }, true)
+
+    window.addEventListener("keydown", ev => {
+        if (config.HOTKEY_ATVIEW(ev)) {
+            ev.preventDefault();
+            ev.stopPropagation();
+            let viewel = []
+            const write = document.getElementById("write");
+            write.children.forEach((element, index) => {
+                if (isinviewbox(element)) { viewel.push(index) }
+            });
+            viewel = [viewel[0] - config.TRUNCATE_LENGTH / 2 > 0 ? viewel[0] - config.TRUNCATE_LENGTH / 2 : 0, viewel.at(-1) + config.TRUNCATE_LENGTH / 2 < write.children.length ? viewel.at(-1) + config.TRUNCATE_LENGTH / 2 : write.children.length]
+            write.children.forEach((element, index) => {
+                if (index < viewel[0] || index > viewel[1]) {
+                    element.style.display = "none";
+                } else {
+                    element.style = "";
+                }
+            });
+
+
+        }
+    }, true)
+
     if (config.DEBUG) {
         JSBridge.invoke("window.toggleDevTools");
     }
 
     console.log("truncate_text.js had been injected");
 })()
+
+function isinviewbox(el) {
+    const totalHeight = window.innerHeight || document.documentElement.clientHeight;
+    const totalWidth = window.innerWidth || document.documentElement.clientWidth;
+    const { top, right, bottom, left } = el.getBoundingClientRect();
+    if (el.style.display) { return false } else { return (top >= 0 && left >= 0 && right <= totalWidth && bottom <= totalHeight); }
+}
+
