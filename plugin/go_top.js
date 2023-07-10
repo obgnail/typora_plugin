6bc671cf6a52862b60a9f319b9544bf18920c416
obgnail <he1251698542@gmail.com>
2023-07-10 22:26:04 +0800

    update go_top.js

--- plugin/go_top.js
+++ plugin/go_top.js
@@ -1,27 +1,36 @@
 (() => {
     const config = {
         ENABLE: true,
+        // 距顶部50像素开始显示
+        THRESHOLD: 50,
     }
 
     if (!config.ENABLE) {
         return
     }
 
-    (() => {
-        const div = document.createElement("div");
-        div.id = 'typora-go-top';
-        div.innerHTML = `<a href="javascript:;" title="返回顶部">goTop</a>`;
-        div.style.position = "fixed";
-        div.style.right = "40px";
-        div.style.bottom = "40px";
-        div.style.zIndex = "99999";
-        const body = document.querySelector("body");
-        body.appendChild(div);
+    const goTopDiv = document.createElement("div");
+    goTopDiv.id = 'typora-go-top';
+    goTopDiv.innerHTML = `<i class="ion-arrow-up-c"></i>`;
+    goTopDiv.style.position = "fixed";
+    goTopDiv.style.right = "50px";
+    goTopDiv.style.bottom = "50px";
+    goTopDiv.style.zIndex = "99999";
+    goTopDiv.style.cursor = "pointer"
+    goTopDiv.style.fontSize = "50px";
+    goTopDiv.style.color = "#ffafa3";
+    goTopDiv.style.display = "none";
+    const body = document.querySelector("body");
+    body.appendChild(goTopDiv);
 
-        document.getElementById("typora-go-top").addEventListener("click", ev => {
-            $("content").animate({scrollTop: '0'}, 600);
-        })
-    })()
+    document.getElementById("typora-go-top").addEventListener("click", ev => {
+        $("content").animate({scrollTop: '0'}, 600);
+    })
+
+    const content = document.querySelector("content");
+    content.addEventListener("scroll", ev => {
+        goTopDiv.style.display = (content.scrollTop > config.THRESHOLD) ? "block" : "none";
+    })
 
     console.log("go_top.js had been injected");
 })()
