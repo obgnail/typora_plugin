ce7467830f6bbc8e1df0c965ccf3cc4022894423
obgnail <he1251698542@gmail.com>
2023-07-12 23:52:30 +0800

    完成div初始化

--- plugin/window_tab/index.js
+++ plugin/window_tab/index.js
@@ -1,13 +1,13 @@
 (() => {
     (() => {
         const css =`
-        .container.svelte-1vhgd3o {
+        #plugin-window-tab .container {
             position: relative;
             width: 100%;
             height: 40px
         }
         
-        .tab-bar-container.svelte-1vhgd3o {
+        #plugin-window-tab .tab-bar-container {
             position: fixed;
             top: 0;
             width: 100%;
@@ -15,12 +15,12 @@
             z-index: 1
         }
         
-        .grab-container.svelte-pb8ij7 {
+        #plugin-window-tab .grab-container {
             height: 100%;
             width: fit-content
         }
         
-        .tab-clone.svelte-pb8ij7 {
+        #plugin-window-tab .tab-clone {
             pointer-events: none;
             width: fit-content;
             height: 40px;
@@ -29,11 +29,11 @@
             z-index: 1000
         }
         
-        .clone-container.svelte-pb8ij7 {
+        #plugin-window-tab .clone-container {
             position: relative
         }
         
-        .container.svelte-pb8ij7 {
+        #plugin-window-tab .tab-bar {
             background-color: var(--bg-color, white);
             height: 100%;
             display: flex;
@@ -43,7 +43,7 @@
             overflow-x: scroll
         }
         
-        .container.svelte-pb8ij7::after {
+        #plugin-window-tab .tab-bar::after {
             content: "";
             height: 100%;
             width: 100vw;
@@ -51,25 +51,25 @@
             border-bottom: solid 1px rgba(0, 0, 0, 0.07)
         }
         
-        .invisible.svelte-pb8ij7 {
+        #plugin-window-tab .invisible {
             opacity: 0
         }
         
-        .container.svelte-pb8ij7:hover::-webkit-scrollbar-thumb {
+        #plugin-window-tab .tab-bar:hover::-webkit-scrollbar-thumb {
             visibility: visible
         }
         
-        .container.svelte-pb8ij7::-webkit-scrollbar {
+        #plugin-window-tab .tab-bar::-webkit-scrollbar {
             height: 5px
         }
         
-        .container.svelte-pb8ij7::-webkit-scrollbar-thumb {
+        #plugin-window-tab .tab-bar::-webkit-scrollbar-thumb {
             height: 5px;
             background-color: var(----active-file-bg-color, gray);
             visibility: hidden
         }
         
-        .container.svelte-1511mcd.svelte-1511mcd {
+        #plugin-window-tab .tab-container {
             background-color: var(--side-bar-bg-color, gray);
             height: 100%;
             min-width: 100px;
@@ -85,7 +85,7 @@
             cursor: pointer
         }
         
-        .name.svelte-1511mcd.svelte-1511mcd {
+        #plugin-window-tab .name {
             max-width: 350px;
             padding-right: 15px;
             font-size: 14px;
@@ -95,7 +95,7 @@
             pointer-events: none
         }
         
-        .close-button.svelte-1511mcd.svelte-1511mcd {
+        #plugin-window-tab .close-button {
             padding: 4px;
             display: flex;
             align-items: center;
@@ -103,11 +103,11 @@
             border-radius: 5px
         }
         
-        .container.svelte-1511mcd:hover>.close-button.svelte-1511mcd {
+        #plugin-window-tab .tab-container:hover>.close-button {
             visibility: visible !important
         }
         
-        .close-icon.svelte-1511mcd.svelte-1511mcd {
+        #plugin-window-tab .close-icon {
             position: relative;
             width: 11px;
             height: 11px;
@@ -116,8 +116,8 @@
             justify-content: center
         }
         
-        .close-icon.svelte-1511mcd.svelte-1511mcd::before,
-        .close-icon.svelte-1511mcd.svelte-1511mcd::after {
+        #plugin-window-tab .close-icon::before,
+        #plugin-window-tab .close-icon::after {
             content: "";
             position: absolute;
             width: 100%;
@@ -125,25 +125,25 @@
             background-color: var(--active-file-border-color, black)
         }
         
-        .close-icon.svelte-1511mcd.svelte-1511mcd::before {
+        #plugin-window-tab .close-icon::before {
             transform: rotate(45deg)
         }
         
-        .close-icon.svelte-1511mcd.svelte-1511mcd::after {
+        #plugin-window-tab .close-icon::after {
             transform: rotate(-45deg)
         }
         
-        .close-button.svelte-1511mcd.svelte-1511mcd:hover {
+        #plugin-window-tab .close-button:hover {
             background-color: var(--active-file-bg-color, lightgray)
         }
         
-        .active.svelte-1511mcd.svelte-1511mcd {
+        #plugin-window-tab .active {
             border: solid 1px rgba(0, 0, 0, 0.07);
             border-bottom: none;
             background-color: var(--bg-color, white)
         }
         
-        .active-indicator.svelte-1511mcd.svelte-1511mcd {
+        #plugin-window-tab .active-indicator {
             position: absolute;
             top: -1px;
             left: -1px;
@@ -152,11 +152,11 @@
             background-color: var(--active-file-border-color, black)
         }
         
-        .preview.svelte-1511mcd.svelte-1511mcd {
+        #plugin-window-tab .preview {
             font-style: italic !important
         }
         
-        .single.svelte-1511mcd.svelte-1511mcd {
+        #plugin-window-tab .single {
             visibility: hidden;
             opacity: 0
         }
@@ -167,16 +167,16 @@
         document.getElementsByTagName("head")[0].appendChild(style);
 
         const div = `
-            <div class="container svelte-1vhgd3o">
-                <div class="tab-bar-container svelte-1vhgd3o">
-                    <div class="clone-container svelte-pb8ij7"></div>
-                    <div class="container svelte-pb8ij7" style="width: calc(100vw - var(--sidebar-width, 0));">
-                        <div class="grab-container svelte-pb8ij7">
-                            <div class="container svelte-1511mcd active">
-                                <div class="active-indicator svelte-1511mcd" style="display: block;"></div> <span
-                                    class="name svelte-1511mcd">messing1.md</span> <span class="close-button svelte-1511mcd"
-                                    style="visibility: visible;">
-                                    <div class="close-icon svelte-1511mcd"></div>
+            <div class="container">
+                <div class="tab-bar-container">
+                    <div class="clone-container"></div>
+                    <div class="tab-bar">
+                        <div class="grab-container">
+                            <div class="tab-container active">
+                                <div class="active-indicator" style="display: block;"></div>
+                                    <span class="name">messing1.md</span>
+                                    <span class="close-button" style="visibility: visible;">
+                                    <div class="close-icon"></div>
                                 </span>
                             </div>
                         </div>
@@ -184,11 +184,11 @@
                 </div>
             </div>
         `
-        const Div = document.createElement("div");
-        Div.id = 'svelte-target';
-        Div.innerHTML = div;
+        const windowTab = document.createElement("div");
+        windowTab.id = "plugin-window-tab";
+        windowTab.innerHTML = div;
         document.getElementById("write-style").parentElement
-            .insertBefore(Div, document.getElementById("write-style"));
+            .insertBefore(windowTab, document.getElementById("write-style"));
     })()
 
     console.log("window_tab.js had been injected");
