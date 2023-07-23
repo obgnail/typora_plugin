95eef6841529db49f594274479b0a14fc5eaa239
obgnail <he1251698542@gmail.com>
2023-07-23 13:42:20 +0800

    add plugin_menu

--- plugin/index.js
+++ plugin/index.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+    global._plugin_had_injected = false;
     global._plugins = [
         {
             name: "标签页管理",
@@ -105,6 +106,14 @@ window.onload = () => {
             call_args: null,
         },
         {
+            name: "右键菜单",
+            src: "./plugin/plugin_menu.js",
+            enable: false,
+            clickable: false,
+            call: null,
+            call_args: null,
+        },
+        {
             name: "测试",
             src: "./plugin/test.js",
             enable: true,
@@ -114,13 +123,13 @@ window.onload = () => {
         },
     ]
 
-    async function loadPlugin() {
+    const loadPlugin = plugins => {
         const _path = reqnode("path");
         const _fs = reqnode("fs");
         const dirname = global.dirname || global.__dirname;
         const promises = [];
 
-        global._plugins.forEach(plugin => {
+        plugins.forEach(plugin => {
             const filepath = _path.join(dirname, plugin.src);
             const promise = new Promise((resolve, reject) => {
                 _fs.access(filepath, err => {
@@ -138,119 +147,8 @@ window.onload = () => {
             promises.push(promise);
         })
 
-        await Promise.all(promises)
+        Promise.all(promises).then(() => global._plugin_had_injected = true)
     }
 
-    const appendMenu = () => {
-        const ul = document.querySelector(`#context-menu`);
-        const line = document.createElement("li");
-        line.classList.add("divider");
-        line.setAttribute("data-group", "plugin");
-        ul.appendChild(line);
-
-        const li = `<li data-key="typora-plugin" data-group="enable-plugin" class="has-extra-menu">
-            <a role="menuitem">
-                <span data-localize="启用插件" data-lg="Menu">启用插件</span>
-                <i class="fa fa-caret-right"></i>
-            </a>
-        </li>`
-        ul.insertAdjacentHTML('beforeend', li);
-
-        const clickablePlugins = [];
-        const notClickablePlugins = [];
-        global._plugins.forEach(plugin => {
-            const style = (plugin.clickable) ? "" : `style="pointer-events: none;color: #c4c6cc;"`;
-            const content = (!plugin.call_args) ? plugin.name : `<span data-lg="Menu">${plugin.name}</span> <i class="fa fa-caret-right"></i>`;
-            const Class = (!plugin.call_args) ? "" : `class="plugin-has-args"`;
-            const li = `<li data-key="${plugin.name}" ${Class} ${style}><a role="menuitem" data-lg="Menu">${content}</a></li>`
-            if (plugin.clickable) {
-                clickablePlugins.push(li);
-            } else {
-                notClickablePlugins.push(li);
-            }
-        })
-
-        const createUl = () => {
-            const secondUl = document.createElement("ul");
-            secondUl.classList.add("dropdown-menu");
-            secondUl.classList.add("context-menu");
-            secondUl.classList.add("ext-context-menu");
-            secondUl.setAttribute("role", "menu");
-            return secondUl;
-        }
-
-        const show = (element, target) => {
-            const selected = $(element).addClass("show")
-                , height = selected.height() + 48
-                , offset = target.offset()
-                , left = offset.left
-                , leftPlus = left + 200
-                , top = offset.top - 30
-                , finalLeft = leftPlus + 160 > window.innerWidth ? left - 184 : leftPlus
-                , finalTop = top + height > window.innerHeight ? window.innerHeight - height : top;
-            selected.css({top: finalTop + "px", left: finalLeft + "px"})
-            return false;
-        };
-
-        setTimeout(() => {
-            const secondUl = createUl();
-            secondUl.id = "plugin-menu"
-            secondUl.innerHTML = `${clickablePlugins.join("")}<li class="divider"></li>${notClickablePlugins.join("")}`
-            document.querySelector("content").appendChild(secondUl);
-
-            const pluginMenu = $("#plugin-menu");
-            pluginMenu.on("click", "[data-key]", function () {
-                const name = this.getAttribute("data-key");
-                const plugins = global._plugins.filter(plugin => plugin.name === name);
-                plugins && plugins[0] && plugins[0].call && plugins[0].call();
-                File.editor.contextMenu.hide();
-            })
-            pluginMenu.on("mouseenter", "[data-key]", function () {
-                const t = $(this);
-                const target = t.find(`span[data-lg="Menu"]`);
-                if (target.length) {
-                    const name = t.attr("data-key");
-                    show(`.plugin-menu-third[plugin_name="${name}"]`, t);
-                } else {
-                    document.querySelectorAll(".plugin-menu-third").forEach(ele => ele.classList.remove("show"));
-                    document.querySelector("#plugin-menu .plugin-has-args").classList.remove("active");
-                }
-            })
-        }, 500)
-
-        setTimeout(() => {
-            global._plugins.forEach(plugin => {
-                if (plugin.call_args) {
-                    const li = plugin.call_args.map(arg => `<li data-key="${arg.arg_name}" arg_value="${arg.arg_value}">
-                            <a role="menuitem" data-lg="Menu">${arg.arg_name}</a></li>`)
-                    const thirdUl = createUl();
-                    thirdUl.classList.add("plugin-menu-third");
-                    thirdUl.setAttribute("plugin_name", plugin.name);
-                    thirdUl.innerHTML = li.join("");
-                    document.querySelector("content").appendChild(thirdUl);
-                }
-            })
-
-            $(".plugin-menu-third").on("click", "[data-key]", function () {
-                const pluginName = this.parentElement.getAttribute("plugin_name");
-                const argValue = this.getAttribute("arg_value");
-                const plugins = global._plugins.filter(plugin => plugin.name === pluginName);
-                plugins && plugins[0] && plugins[0].call && plugins[0].call(argValue);
-                File.editor.contextMenu.hide();
-            })
-        }, 500)
-
-        $("#context-menu").on("mouseenter", "[data-key]", function () {
-            const target = $(this);
-            if ("typora-plugin" === target.attr("data-key")) {
-                show("#plugin-menu", target);
-                target.addClass("active");
-            } else {
-                document.querySelector("#plugin-menu").classList.remove("show");
-                document.querySelector("[data-key='typora-plugin']").classList.remove("active");
-            }
-        })
-    }
-
-    loadPlugin().then(appendMenu);
+    loadPlugin(global._plugins);
 }
