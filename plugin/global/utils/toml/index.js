29329317d62184dcc5b76c4f7c8ab9cdcfd4f856
obgnail <he1251698542@gmail.com>
2023-08-26 00:52:22 +0800

    add toml stringify

--- plugin/global/utils/toml/index.js
+++ plugin/global/utils/toml/index.js
@@ -1,9 +1,11 @@
 var parser = require('./parser');
 var compiler = require('./compiler');
+var stringify = require('./stringify');
 
 module.exports = {
   parse: function(input) {
     var nodes = parser.parse(input.toString());
     return compiler.compile(nodes);
-  }
+  },
+  stringify,
 };
