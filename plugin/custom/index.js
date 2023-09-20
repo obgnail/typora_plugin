6c5049c0615f6f7caa48c5b865d66befa5652ce7
obgnail <he1251698542@gmail.com>
2023-09-20 22:24:23 +0800

    move modal helper

--- plugin/custom/index.js
+++ plugin/custom/index.js
@@ -1,17 +1,12 @@
 class CustomPlugin extends global._basePlugin {
     beforeProcess = () => {
         this.custom = {};
-        this.modalHelper = new modalHelper(this.custom, this.utils);
         this.hotkeyHelper = new hotkeyHelper(this.custom);
         this.dynamicCallHelper = new dynamicCallHelper(this.custom);
         this.loadPluginHelper = new loadPluginHelper(this);
         this.loadPluginHelper.load();
     }
-    style = () => this.modalHelper.style()
-    html = () => this.modalHelper.html()
     hotkey = () => this.hotkeyHelper.hotkey()
-    modal = (customPlugin, modal, callback, cancelCallback) => this.modalHelper.modal(customPlugin, modal, callback, cancelCallback)
-    process = () => this.modalHelper.process()
     dynamicCallArgsGenerator = anchorNode => this.dynamicCallHelper.dynamicCallArgsGenerator(anchorNode)
     call = name => this.dynamicCallHelper.call(name)
 }
@@ -172,188 +167,18 @@ class hotkeyHelper {
     }
 }
 
-class modalHelper {
-    constructor(custom, utils) {
-        this.custom = custom;
-        this.utils = utils;
-        this.pluginModal = null;
-        this.callback = null;
-        this.entities = null;
-    }
-
-    style = () => {
-        return `
-            #plugin-custom-modal {
-                position: fixed;
-                z-index: 99999;
-                margin: 50px auto;
-                left: 0;
-                right: 0;
-                display: none;
-            }
-            
-            #plugin-custom-modal label {
-                display: block;
-                margin-bottom: 5px;
-            }
-            
-            #plugin-custom-modal input[type="checkbox"], input[type="radio"] {
-                box-shadow: none;
-                margin-top: -3px;
-            }
-        `
-    }
-
-    html = () => {
-        const modal_content = `
-            <div class="modal-content">
-              <div class="modal-header">
-                <div class="modal-title" data-lg="Front">自定义插件弹窗</div>
-              </div>
-              <div class="modal-body"></div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default plugin-modal-cancel" data-dismiss="modal" data-lg="Front">取消</button>
-                <button type="button" class="btn btn-primary plugin-modal-submit" data-lg="Front">确定</button>
-              </div>
-            </div>
-        `
-        const modal = document.createElement("div");
-        modal.id = "plugin-custom-modal";
-        modal.classList.add("modal-dialog");
-        modal.innerHTML = modal_content;
-        this.utils.insertDiv(modal);
-    }
-
-    process = () => {
-        this.entities = {
-            modal: document.getElementById("plugin-custom-modal"),
-            content: document.querySelector("#plugin-custom-modal .modal-content"),
-            body: document.querySelector("#plugin-custom-modal .modal-body"),
-            title: document.querySelector("#plugin-custom-modal .modal-title"),
-            submit: document.querySelector("#plugin-custom-modal button.plugin-modal-submit"),
-            cancel: document.querySelector("#plugin-custom-modal button.plugin-modal-cancel"),
-        }
-
-        this.entities.cancel.addEventListener("click", () => this.onButtonClick(this.cancelCallback))
-        this.entities.submit.addEventListener("click", () => this.onButtonClick(this.callback))
-        this.entities.modal.addEventListener("keydown", ev => {
-            if (ev.key === "Enter") {
-                this.entities.submit.click();
-                ev.stopPropagation();
-                ev.preventDefault();
-            } else if (ev.key === "Escape") {
-                this.entities.cancel.click();
-                ev.stopPropagation();
-                ev.preventDefault();
-            }
-        }, true)
-    }
-
-    onButtonClick = callback => {
-        const name = this.entities.content.getAttribute("custom-plugin-name");
-        const plugin = this.custom[name];
-        if (!plugin) return;
-
-        this.pluginModal.components.forEach(component => {
-            if (!component.label || !component.type || !component.id) return;
-            const div = this.entities.body.querySelector(`.form-group[component-id="${component.id}"]`);
-            if (div) {
-                component.submit = this.getWidgetValue(component.type, div);
-            }
-        })
-        callback && callback(this.pluginModal.components);
-        this.entities.modal.style.display = "none";
-    }
-
-    getWidgetValue = (type, widget) => {
-        switch (type.toLowerCase()) {
-            case "input":
-                return widget.querySelector("input").value
-            case "textarea":
-                return widget.querySelector("textarea").value
-            case "checkbox":
-                return [...widget.querySelectorAll("input:checked")].map(box => box.value)
-            case "radio":
-                return widget.querySelector("input:checked").value
-            case "select":
-                return widget.querySelector("select").value
-            case "file":
-                return widget.querySelector("input").files
-        }
-    }
-
-    newWidget = component => {
-        if (!component || !component.label || !component.type) return;
-
-        let inner = "";
-        const type = component.type.toLowerCase();
-        switch (type) {
-            case "input":
-            case "password":
-            case "file":
-                inner = `<input type="${type === "input" ? "text" : type}" class="form-control" 
-                            placeholder="${component.placeholder}" value="${component.value}">`;
-                break
-            case "textarea":
-                const rows = component.rows || 3;
-                inner = `<textarea class="form-control" rows="${rows}" placeholder="${component.placeholder}"></textarea>`;
-                break
-            case "checkbox":
-                const checkBoxList = component.list.map(box => `
-                    <div class="checkbox">
-                        <label><input type="checkbox" value="${box.value}" ${box.checked ? "checked" : ""}>${box.label}</label>
-                    </div>`
-                );
-                inner = checkBoxList.join("");
-                break
-            case "radio":
-                const radioList = component.list.map(radio => `
-                    <div class="radio">
-                        <label><input type="radio" name="radio-${component.id}" value="${radio.value}" ${radio.checked ? "checked" : ""}>${radio.label}</label>
-                    </div>`
-                );
-                inner = radioList.join("");
-                break
-            case "select":
-                const optionsList = component.list.map(option => `<option ${option === component.selected ? "selected" : ""}>${option}</option>`);
-                inner = `<select class="form-control">${optionsList}</select>`
-                break
-            case "p":
-                break
-        }
-        return `<div class="col-lg-12 form-group" component-id="${component.id}"><label>${component.label}</label>${inner}</div>`;
-    }
-
-    // modal: {title: "", components: [{name: "", type: "", value: ""}]}
-    modal = (customPlugin, modal, callback, cancelCallback) => {
-        if (customPlugin && customPlugin["name"] && modal && callback instanceof Function) {
-            this.pluginModal = modal;
-            this.callback = callback;
-            this.cancelCallback = cancelCallback;
-
-            this.entities.content.setAttribute("custom-plugin-name", customPlugin.name);
-            this.entities.title.innerText = modal.title;
-            modal.components.forEach(component => component.id = Math.random());
-            const widgetList = modal.components.map(component => this.newWidget(component));
-            this.entities.body.innerHTML = `<form role="form">` + widgetList.join("") + "</form>";
-            this.entities.modal.style.display = "block";
-        }
-    }
-}
-
 class BaseCustomPlugin {
     constructor(info, controller) {
         this.info = info;
         this.showName = info.name;
         this.name = info.plugin;
+        this.fixedName = info.plugin;
         this.config = info.config;
         this.utils = controller.utils;
         this.controller = controller;
     }
 
-    modal(pluginModal, callback, cancelCallback) {
-        this.controller.modal(this, pluginModal, callback, cancelCallback);
-    }
+    modal = (pluginModal, callback, cancelCallback) => this.utils.modal(pluginModal, callback, cancelCallback);
 
     init = () => {
     }
--- plugin/global/core/plugin.js
+++ plugin/global/core/plugin.js
@@ -132,6 +132,8 @@ class utils {
 
     static registerStateRecorder = (selector, stateGetter, stateRestorer) => global._stateRecorder.register(selector, stateGetter, stateRestorer);
 
+    static modal = (modal, callback, cancelCallback) => global._modalHelper.modal(modal, callback, cancelCallback);
+
     static insertStyle = (id, css) => {
         const style = document.createElement('style');
         style.id = id;
@@ -930,6 +932,172 @@ class stateRecorder {
     }
 }
 
+class modalHelper {
+    constructor() {
+        this.utils = utils;
+        this.pluginModal = null;
+        this.callback = null;
+        this.entities = null;
+    }
+
+    style = () => {
+        return `
+            #plugin-custom-modal {
+                position: fixed;
+                z-index: 99999;
+                margin: 50px auto;
+                left: 0;
+                right: 0;
+                display: none;
+            }
+            
+            #plugin-custom-modal label {
+                display: block;
+                margin-bottom: 5px;
+            }
+            
+            #plugin-custom-modal input[type="checkbox"], input[type="radio"] {
+                box-shadow: none;
+                margin-top: -3px;
+            }
+        `
+    }
+
+    html = () => {
+        const modal_content = `
+            <div class="modal-content">
+              <div class="modal-header">
+                <div class="modal-title" data-lg="Front">自定义插件弹窗</div>
+              </div>
+              <div class="modal-body"></div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default plugin-modal-cancel" data-dismiss="modal" data-lg="Front">取消</button>
+                <button type="button" class="btn btn-primary plugin-modal-submit" data-lg="Front">确定</button>
+              </div>
+            </div>
+        `
+        const modal = document.createElement("div");
+        modal.id = "plugin-custom-modal";
+        modal.classList.add("modal-dialog");
+        modal.innerHTML = modal_content;
+        this.utils.insertDiv(modal);
+    }
+
+    process = () => {
+        this.utils.insertStyle("plugin-custom-modal-style", this.style());
+        this.html();
+
+        this.entities = {
+            modal: document.getElementById("plugin-custom-modal"),
+            content: document.querySelector("#plugin-custom-modal .modal-content"),
+            body: document.querySelector("#plugin-custom-modal .modal-body"),
+            title: document.querySelector("#plugin-custom-modal .modal-title"),
+            submit: document.querySelector("#plugin-custom-modal button.plugin-modal-submit"),
+            cancel: document.querySelector("#plugin-custom-modal button.plugin-modal-cancel"),
+        }
+
+        this.entities.cancel.addEventListener("click", () => this.onButtonClick(this.cancelCallback))
+        this.entities.submit.addEventListener("click", () => this.onButtonClick(this.callback))
+        this.entities.modal.addEventListener("keydown", ev => {
+            if (ev.key === "Enter") {
+                this.entities.submit.click();
+                ev.stopPropagation();
+                ev.preventDefault();
+            } else if (ev.key === "Escape") {
+                this.entities.cancel.click();
+                ev.stopPropagation();
+                ev.preventDefault();
+            }
+        }, true)
+    }
+
+    onButtonClick = callback => {
+        this.pluginModal.components.forEach(component => {
+            if (!component.label || !component.type || !component.id) return;
+            const div = this.entities.body.querySelector(`.form-group[component-id="${component.id}"]`);
+            if (div) {
+                component.submit = this.getWidgetValue(component.type, div);
+            }
+        })
+        callback && callback(this.pluginModal.components);
+        this.entities.modal.style.display = "none";
+    }
+
+    getWidgetValue = (type, widget) => {
+        switch (type.toLowerCase()) {
+            case "input":
+                return widget.querySelector("input").value
+            case "textarea":
+                return widget.querySelector("textarea").value
+            case "checkbox":
+                return [...widget.querySelectorAll("input:checked")].map(box => box.value)
+            case "radio":
+                return widget.querySelector("input:checked").value
+            case "select":
+                return widget.querySelector("select").value
+            case "file":
+                return widget.querySelector("input").files
+        }
+    }
+
+    newWidget = component => {
+        if (!component || !component.label || !component.type) return;
+
+        let inner = "";
+        const type = component.type.toLowerCase();
+        switch (type) {
+            case "input":
+            case "password":
+            case "file":
+                inner = `<input type="${type === "input" ? "text" : type}" class="form-control" 
+                            placeholder="${component.placeholder}" value="${component.value}">`;
+                break
+            case "textarea":
+                const rows = component.rows || 3;
+                inner = `<textarea class="form-control" rows="${rows}" placeholder="${component.placeholder}"></textarea>`;
+                break
+            case "checkbox":
+                const checkBoxList = component.list.map(box => `
+                    <div class="checkbox">
+                        <label><input type="checkbox" value="${box.value}" ${box.checked ? "checked" : ""}>${box.label}</label>
+                    </div>`
+                );
+                inner = checkBoxList.join("");
+                break
+            case "radio":
+                const radioList = component.list.map(radio => `
+                    <div class="radio">
+                        <label><input type="radio" name="radio-${component.id}" value="${radio.value}" ${radio.checked ? "checked" : ""}>${radio.label}</label>
+                    </div>`
+                );
+                inner = radioList.join("");
+                break
+            case "select":
+                const optionsList = component.list.map(option => `<option ${option === component.selected ? "selected" : ""}>${option}</option>`);
+                inner = `<select class="form-control">${optionsList}</select>`
+                break
+            case "p":
+                break
+        }
+        return `<div class="col-lg-12 form-group" component-id="${component.id}"><label>${component.label}</label>${inner}</div>`;
+    }
+
+    // modal: {title: "", components: [{label: "", type: "", value: ""}]}
+    modal = (modal, callback, cancelCallback) => {
+        if (modal && callback instanceof Function) {
+            this.pluginModal = modal;
+            this.callback = callback;
+            this.cancelCallback = cancelCallback;
+
+            this.entities.title.innerText = modal.title;
+            modal.components.forEach(component => component.id = Math.random());
+            const widgetList = modal.components.map(component => this.newWidget(component));
+            this.entities.body.innerHTML = `<form role="form">` + widgetList.join("") + "</form>";
+            this.entities.modal.style.display = "block";
+        }
+    }
+}
+
 class basePlugin {
     constructor(setting) {
         this.fixed_name = setting.fixed_name;
@@ -1092,6 +1260,7 @@ class process {
             global._eventHub.process();
             global._diagramParser.process();
             global._stateRecorder.process();
+            global._modalHelper.process();
             this.hotkeyHelper.listen();
             this.utils.publishEvent(this.utils.eventType.allPluginsHadInjected);
         })
@@ -1104,6 +1273,7 @@ global._hotkeyHelper = hotkeyHelper;
 global._diagramParser = new DiagramParser();
 global._eventHub = new EventHub();
 global._stateRecorder = new stateRecorder();
+global._modalHelper = new modalHelper();
 
 module.exports = {
     process
