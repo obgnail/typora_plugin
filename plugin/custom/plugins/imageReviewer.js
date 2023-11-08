85825390dfdcc04d9a3e918a8a0e5c65deff30d1
obgnail <he1251698542@gmail.com>
2023-11-08 20:42:48 +0800

    update imageReviewer

--- plugin/custom/plugins/imageReviewer.js
+++ plugin/custom/plugins/imageReviewer.js
@@ -9,22 +9,23 @@ class imageReviewerPlugin extends BaseCustomPlugin {
         const messages = [{class_: "review-index"}, {class_: "review-title"}, {class_: "review-size"}];
         const options = [
             {ele: "i", class_: "fa fa-question-circle", title: this.optionHint()},
-            {ele: "i", class_: "fa fa-download", option: "download", title: "下载图片"},
+            {ele: "i", class_: "fa fa-download", option: "download", title: "下载网络图片"},
+            {ele: "i", class_: "fa fa-play", option: "play", title: "图片轮播"},
             {ele: "i", class_: "fa fa-arrows-v", option: "vFlip", title: "垂直翻转"},
             {ele: "i", class_: "fa fa-arrows-h", option: "hFlip", title: "水平翻转"},
             {ele: "i", class_: "fa fa-search-plus", option: "autoSize", title: "放缩"},
             {ele: "i", class_: "fa fa-rotate-right", option: "rotateRight", title: "旋转"},
-            {ele: "i", class_: "fa fa-crosshairs", option: "target", title: "定位到文档"},
+            {ele: "i", class_: "fa fa-crosshairs", option: "scroll", title: "定位到文档"},
             {ele: "i", class_: "fa fa-location-arrow", option: "location", title: "资源管理器打开"},
             {ele: "i", class_: "fa fa-times", option: "close", title: "退出"},
         ]
-        const tool = [{class_: "review-message", children: messages}, {class_: "review-options", children: options}];
+        const tools = [{class_: "review-message", children: messages}, {class_: "review-options", children: options}];
         const children = [
             {class_: "mask plugin-cover-content"},
             {ele: "img", class_: "review-image"},
             {class_: "review-item", action: "get-previous", children: [{ele: "i", class_: "fa fa-angle-left"}]},
             {class_: "review-item", action: "get-next", children: [{ele: "i", class_: "fa fa-angle-right"}]},
-            {class_: "review-tool", children: tool}
+            {class_: "review-tool", children: tools}
         ]
         return [{id: "plugin-image-reviewer", class_: "plugin-cover-content", children}]
     }
@@ -33,14 +34,14 @@ class imageReviewerPlugin extends BaseCustomPlugin {
 
     callback = () => {
         if (this.entities.reviewer.style.display === "") {
-            this.entities.reviewer.style.display = "block";
-            this.showImage();
+            this.show();
         } else {
             this.close();
         }
     }
 
     process = () => {
+        this.playTimer = null;
         this.entities = {
             reviewer: document.getElementById("plugin-image-reviewer"),
             mask: document.querySelector("#plugin-image-reviewer .mask"),
@@ -128,6 +129,15 @@ class imageReviewerPlugin extends BaseCustomPlugin {
         return `skew${direction}(${newSkew}deg)`
     })
 
+    translate = (isOut, direction, newTranslate, translateScale) => this.replaceImageTransform(new RegExp(`translate${direction}\\((.*?)px\\)`), (_, curTranslate) => {
+        if (!newTranslate) {
+            const currentTranslate = parseFloat(curTranslate);
+            translateScale = translateScale || this.config.translate_scale;
+            newTranslate = isOut ? currentTranslate + translateScale : currentTranslate - translateScale;
+        }
+        return `translate${direction}(${newTranslate}px)`
+    }, false)
+
     flip = direction => this.replaceImageTransform(new RegExp(`scale${direction}\\((.*?)\\)`), (_, curScale) => {
         const currentScale = parseInt(curScale);
         return `scale${direction}(${-currentScale})`
@@ -142,13 +152,6 @@ class imageReviewerPlugin extends BaseCustomPlugin {
         this.zoom(null, 1);
     }
 
-    restore = () => {
-        this.entities.image.style.maxWidth = "";
-        this.entities.image.style.maxHeight = "";
-        this.entities.image.style.transform = "scale(1) rotate(0deg) scaleX(1) scaleY(1) skewX(0deg) skewY(0deg)";
-        this.moveImageCenter();
-    }
-
     moveImageCenter = () => {
         const {width, height} = this.entities.mask.getBoundingClientRect();
         const {width: imageWidth, height: imageHeight} = this.entities.image;
@@ -158,8 +161,28 @@ class imageReviewerPlugin extends BaseCustomPlugin {
 
     showImage = (next = true) => {
         this.initImageMsgGetter();
-        const {src, alt, naturalWidth, naturalHeight, idx, total} = this.imageGetter(next);
-        this.entities.msg.querySelector(".review-index").textContent = `[ ${idx} / ${total} ]`;
+        const imgInfo = this.imageGetter(next);
+        this._showImage(imgInfo);
+    }
+
+    dumpIndex = targetIdx => {
+        this.initImageMsgGetter();
+        let imgInfo = this.imageGetter(true);
+        if (!Number.isInteger(targetIdx)) {
+            targetIdx = 0;
+        }
+        targetIdx++;
+        targetIdx = Math.max(targetIdx, 1);
+        targetIdx = Math.min(targetIdx, imgInfo.total);
+        while (imgInfo.showIdx !== targetIdx) {
+            imgInfo = this.imageGetter(true);
+        }
+        this._showImage(imgInfo);
+    }
+
+    _showImage = imgInfo => {
+        const {src, alt, naturalWidth, naturalHeight, showIdx, total} = imgInfo;
+        this.entities.msg.querySelector(".review-index").textContent = `[ ${showIdx} / ${total} ]`;
         this.entities.msg.querySelector(".review-title").textContent = alt;
         this.entities.msg.querySelector(".review-size").textContent = `${naturalWidth} × ${naturalHeight}`;
         this.entities.ops.querySelector(`[option="autoSize"]`).className = "fa fa-search-plus";
@@ -168,27 +191,27 @@ class imageReviewerPlugin extends BaseCustomPlugin {
         this.restore();
     }
 
-    closestViewBoxImage = imageList => {
-        let closestImg = null;
-        let minDistance = Number.MAX_VALUE;
-        imageList.forEach(img => {
-            const distance = Math.abs(img.getBoundingClientRect().top - window.innerHeight / 2);
-            if (distance < minDistance) {
-                minDistance = distance;
-                closestImg = img;
-            }
-        });
-        return closestImg
-    }
-
-    inViewBoxImage = imageList => imageList.find(img => this.utils.isInViewBox(img))
-
-    firstImage = imageList => imageList[0]
-
     getImage = imageList => {
+        const strategyFuncList = {
+            firstImage: imageList => imageList[0],
+            inViewBoxImage: imageList => imageList.find(img => this.utils.isInViewBox(img)),
+            closestViewBoxImage: imageList => {
+                let closestImg = null;
+                let minDistance = Number.MAX_VALUE;
+                imageList.forEach(img => {
+                    const distance = Math.abs(img.getBoundingClientRect().top - window.innerHeight / 2);
+                    if (distance < minDistance) {
+                        minDistance = distance;
+                        closestImg = img;
+                    }
+                });
+                return closestImg
+            }
+        }
         const strategies = [...this.config.first_image_strategies, "firstImage"];
         for (const strategy of strategies) {
-            const image = this[strategy](imageList);
+            const func = strategyFuncList[strategy] || strategyFuncList.firstImage;
+            const image = func(imageList);
             if (image) {
                 return image
             }
@@ -206,7 +229,7 @@ class imageReviewerPlugin extends BaseCustomPlugin {
         if (!target) return;
 
         while (true) {
-            const {img, idx, total} = this.imageGetter(true);
+            const {img, showIdx, total} = this.imageGetter(true);
             if (!img) return;
 
             if (img === target) {
@@ -214,7 +237,7 @@ class imageReviewerPlugin extends BaseCustomPlugin {
                 return
             }
             // 防御代码，防止死循环
-            if (idx === total) {
+            if (showIdx === total) {
                 return img;
             }
         }
@@ -238,12 +261,111 @@ class imageReviewerPlugin extends BaseCustomPlugin {
                 alt: img && img.getAttribute("alt") || "",
                 naturalWidth: img && img.naturalWidth || 0,
                 naturalHeight: img && img.naturalHeight || 0,
-                idx: showIdx,
+                showIdx,
                 total: imageList.length || 0,
             };
         }
     }
 
+    optionHint = () => {
+        const translate = {
+            dummy: '无功能',
+            close: '关闭',
+            download: '下载网络图片',
+            scroll: '定位到文档',
+            play: '轮播图片',
+            location: '打开图片路径',
+            nextImage: '下张图',
+            previousImage: '上张图',
+            firstImage: '第一张图',
+            lastImage: '最后一张图',
+            zoomOut: '缩小图片',
+            zoomIn: '放大图片',
+            rotateLeft: '图片向左旋转',
+            rotateRight: '图片向右旋转',
+            hFlip: '水平翻转图片',
+            vFlip: '垂直翻转图片',
+            translateLeft: '向左移动',
+            translateRight: '向右移动',
+            translateUp: '向上移动',
+            translateDown: '向下移动',
+            incHSkew: '图片增大水平倾斜',
+            decHSkew: '图片减小水平倾斜',
+            incVSkew: '图片增大垂直倾斜',
+            decVSkew: '图片减小垂直倾斜',
+            originSize: '还原图片大小',
+            fixScreen: '图片大小适配屏幕',
+            autoSize: '图片大小切换',
+            restore: '图片恢复为最初状态',
+        }
+        const keyTranslate = {arrowup: '↑', arrowdown: '↓', arrowleft: '←', arrowright: '→'};
+
+        const result = ["当前的配置如下："];
+        const button = ["mousedown_function", "wheel_function"];
+        const extra = ["", "ctrl", "shift", "alt"];
+        button.forEach(btn => extra.forEach(ex => {
+            const cfg = !ex ? btn : ex + "_" + btn;
+            const config = this.config[cfg];
+            const funcList = (btn === "mousedown_function") ? ["鼠标左键", "鼠标中键", "鼠标右键"] : ["滚轮上滚", "滚轮下滚"];
+            funcList.forEach((ele, idx) => {
+                const info = translate[config[idx]];
+                if (info && info !== "无功能") {
+                    const ex_ = !ex ? "" : ex + "+";
+                    result.push(ex_ + ele + "\t" + info);
+                }
+            })
+        }))
+        this.config.hotkey_function.forEach(item => {
+            const [key, func] = item;
+            const info = translate[func];
+            if (info && info !== "无功能") {
+                const translateKey = keyTranslate[key.toLowerCase()] || key;
+                result.push(translateKey + "\t" + info);
+            }
+        })
+        return result.join("\n")
+    }
+
+    handleBlurBackground = (remove = false) => {
+        if (this.config.blur_level === 0) return;
+        const func = remove
+            ? ele => ele && ele.style.removeProperty("filter")
+            : ele => ele && (ele.style.filter = `blur(${this.config.blur_level}px)`);
+        const list = [
+            document.querySelector("#write"),
+            document.querySelector(".sidebar-menu"),
+            document.querySelector("#plugin-window-tab"),
+        ];
+        list.forEach(func);
+    }
+
+    handleHotkey = (remove = false) => {
+        const func = remove
+            ? item => this.utils.unregisterHotkey(item[0])
+            : item => this.utils.registerSingleHotkey(item[0], this[item[1]] || this.dummy);
+        this.config.hotkey_function.forEach(func);
+    }
+
+    handlePlayTimer = (stop = false) => {
+        const btn = this.entities.ops.querySelector(`[option="play"]`);
+        if (!btn) return;
+        if (!stop && !this.playTimer) {
+            this.playTimer = setInterval(this.showImage, this.config.play_second * 1000);
+            btn.classList.add("active");
+        } else {
+            btn.classList.remove("active");
+            clearInterval(this.playTimer);
+            this.playTimer = null;
+        }
+    }
+
+    play = () => this.handlePlayTimer(!!this.playTimer)
+    restore = () => {
+        this.entities.image.style.maxWidth = "";
+        this.entities.image.style.maxHeight = "";
+        this.entities.image.style.transform = "scale(1) rotate(0deg) scaleX(1) scaleY(1) skewX(0deg) skewY(0deg) translateX(0px) translateY(0px)";
+        this.moveImageCenter();
+    }
     location = () => {
         let src = this.entities.image.getAttribute("src");
         if (this.utils.isNetworkImage(src)) {
@@ -256,7 +378,6 @@ class imageReviewerPlugin extends BaseCustomPlugin {
             JSBridge.showInFinder(src);
         }
     }
-
     download = async () => {
         const src = this.entities.image.getAttribute("src");
         if (!this.utils.isNetworkImage(src)) return;
@@ -267,73 +388,46 @@ class imageReviewerPlugin extends BaseCustomPlugin {
             alert("download image failed");
         }
     }
-
-    optionHint = () => {
-        const translate = {
-            dummy: '无功能',
-            close: '关闭',
-            location: '打开图片路径',
-            nextImage: '下张图',
-            previousImage: '上张图',
-            zoomOut: '放大图片',
-            zoomIn: '缩小图片',
-            rotateLeft: '图片向左旋转',
-            rotateRight: '图片向右旋转',
-            hFlip: '水平翻转图片',
-            vFlip: '垂直翻转图片',
-            incHSkew: '图片增大水平倾斜',
-            decHSkew: '图片减小水平倾斜',
-            incVSkew: '图片增大垂直倾斜',
-            decVSkew: '图片减小垂直倾斜',
-            originSize: '还原图片大小',
-            fixScreen: '图片大小适配屏幕',
-            autoSize: '图片大小切换',
-            restore: '图片恢复为最初状态',
-        }
-
-        const result = [];
-        const button = ["mousedown_function", "wheel_function"];
-        const extra = ["", "ctrl", "shift", "alt"];
-        button.forEach(btn => extra.forEach(ex => {
-            const cfg = !ex ? btn : ex + "_" + btn;
-            const config = this.config[cfg];
-            const funcs = (btn === "mousedown_function") ? ["鼠标左键", "鼠标中键", "鼠标右键"] : ["滚轮上滚", "滚轮下滚"];
-            funcs.forEach((ele, idx) => {
-                const info = translate[config[idx]];
-                if (info !== "无功能") {
-                    const ex_ = !ex ? "" : ex + "+";
-                    result.push(ex_ + ele + "\t" + info);
-                }
-            })
-        }))
-        return result.join("\n")
-    }
-
-    target = () => {
+    scroll = () => {
         const text = this.entities.msg.querySelector(".review-index").textContent;
         const idx = parseInt(text.substring(1, text.indexOf("/")));
         const image = Array.from(document.querySelectorAll("#write img"))[idx - 1];
         this.close();
         image && this.utils.scroll(image, 30);
     }
-
+    show = () => {
+        document.activeElement.blur();
+        this.handleBlurBackground(false);
+        this.handleHotkey(false);
+        this.entities.reviewer.style.display = "block";
+        this.showImage();
+    }
     close = () => {
+        this.handleBlurBackground(true);
+        this.handleHotkey(true);
+        this.handlePlayTimer(true);
         this.entities.reviewer.style.display = "";
         this.imageGetter = null;
     }
     dummy = () => null
     nextImage = () => this.showImage(true)
     previousImage = () => this.showImage(false)
+    firstImage = () => this.dumpIndex(-1)
+    lastImage = () => this.dumpIndex(Number.MAX_VALUE)
     rotateLeft = rotateScale => this.rotate(false, null, rotateScale)
     rotateRight = rotateScale => this.rotate(true, null, rotateScale)
-    zoomOut = zoomScale => this.zoom(true, null, zoomScale)
-    zoomIn = zoomScale => this.zoom(false, null, zoomScale)
+    zoomIn = zoomScale => this.zoom(true, null, zoomScale)
+    zoomOut = zoomScale => this.zoom(false, null, zoomScale)
     hFlip = () => this.flip("X")
     vFlip = () => this.flip("Y")
     incHSkew = skewScale => this.skew(true, "X", null, skewScale)
     decHSkew = skewScale => this.skew(false, "X", null, skewScale)
     incVSkew = skewScale => this.skew(true, "Y", null, skewScale)
     decVSkew = skewScale => this.skew(false, "Y", null, skewScale)
+    translateLeft = translateScale => this.translate(false, "X", null, translateScale)
+    translateRight = translateScale => this.translate(true, "X", null, translateScale)
+    translateUp = translateScale => this.translate(false, "Y", null, translateScale)
+    translateDown = translateScale => this.translate(true, "Y", null, translateScale)
     originSize = () => this.changeSize(true)
     fixScreen = () => this.changeSize(false)
     autoSize = () => this.changeSize(this.entities.image.style.maxWidth !== "initial")
