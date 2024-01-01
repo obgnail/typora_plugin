8db7270428c0ac4e077049165495624281d47946
obgnail <he1251698542@gmail.com>
2024-01-01 18:44:13 +0800

    update md_padding

--- plugin/md_padding/index.js
+++ plugin/md_padding/index.js
@@ -8,11 +8,21 @@ class mdPaddingPlugin extends BasePlugin {
         return padMarkdown(content, {ignoreWords: this.config.IGNORE_WORDS})
     }
 
+    removeMultiLineBreak = content => {
+        const lineBreak = content.indexOf("\r\n") !== -1 ? "\r\n" : "\n";
+        const reg = new RegExp(`(${lineBreak}){3,}`, "g");
+        const double = lineBreak + lineBreak;
+        return content.replace(reg, double);
+    }
+
     call = async () => {
         await File.saveUseNode();
         const filepath = this.utils.getFilePath();
         const content = await this.utils.Package.Fs.promises.readFile(filepath, 'utf-8');
-        const formattedContent = this.formatFile(content);
+        let formattedContent = this.formatFile(content);
+        if (this.config.REMOVE_MULTI_LINE_BREAK) {
+            formattedContent = this.removeMultiLineBreak(formattedContent);
+        }
         await this.utils.Package.Fs.promises.writeFile(filepath, formattedContent);
         this.reload(formattedContent);
     }
