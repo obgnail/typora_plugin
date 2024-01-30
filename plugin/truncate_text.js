d45dff7a3d904b9598defbeba09b0630c69902f0
obgnail <he1251698542@gmail.com>
2024-01-30 12:58:29 +0800

    update truncate_text

--- plugin/truncate_text.js
+++ plugin/truncate_text.js
@@ -1,6 +1,11 @@
 class truncateTextPlugin extends BasePlugin {
     beforeProcess = () => {
         this.className = "plugin-truncate-text";
+        this.callArgs = [
+            {arg_name: `只保留最后${this.config.REMAIN_LENGTH}段`, arg_value: "hide_front"},
+            {arg_name: "重新显示所有内容", arg_value: "show_all"},
+            {arg_name: "根据当前可视范围显示", arg_value: "hide_base_view"}
+        ];
     }
 
     hotkey = () => [
@@ -64,20 +69,6 @@ class truncateTextPlugin extends BasePlugin {
         }
     };
 
-    // // 已废弃
-    // rollback2 = start => {
-    //     if (document.querySelector(`#write > .${this.className}`)) {
-    //         let ele = start.closest("#write > [cid]");
-    //         while (ele) {
-    //             if (ele.classList.contains(this.className)) {
-    //                 ele.classList.remove(this.className);
-    //                 ele.style.display = "";
-    //             }
-    //             ele = ele.nextElementSibling;
-    //         }
-    //     }
-    // }
-
     call = type => {
         if (type === "hide_front") {
             this.hideFront();
@@ -88,12 +79,6 @@ class truncateTextPlugin extends BasePlugin {
         }
         this.callbackOtherPlugin();
     }
-
-    callArgs = [
-        {arg_name: `只保留最后${this.config.REMAIN_LENGTH}段`, arg_value: "hide_front"},
-        {arg_name: "重新显示所有内容", arg_value: "show_all"},
-        {arg_name: "根据当前可视范围显示", arg_value: "hide_base_view"}
-    ];
 }
 
 module.exports = {
