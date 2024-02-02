c82d16679041f9c3307cb5fdb3c4ce5e7b3318a4
obgnail <he1251698542@gmail.com>
2024-02-02 18:58:59 +0800

    update toc

--- plugin/custom/plugins/toc.js
+++ plugin/custom/plugins/toc.js
@@ -1,12 +1,24 @@
 class tocPlugin extends BaseCustomPlugin {
     styleTemplate = () => true
-    htmlTemplate = () => [{id: "plugin-toc", class_: "plugin-common-modal plugin-toc", style: {display: "none"}}]
+    htmlTemplate = () => [{
+        id: "plugin-toc",
+        class_: "plugin-common-modal plugin-toc",
+        style: {display: "none"},
+        children: [{class_: "grip-right"}, {class_: "toc-ul"}]
+    }]
     hotkey = () => [this.config.hotkey]
 
     process = () => {
-        this.toc = document.querySelector("#plugin-toc");
-        this.utils.addEventListener(this.utils.eventType.outlineUpdated, () => this.toc.style.display === "block" && this.renewModal());
-        this.toc.addEventListener("click", ev => {
+        this.entities = {
+            content: document.querySelector("content"),
+            modal: document.querySelector("#plugin-toc"),
+            grip: document.querySelector("#plugin-toc .grip-right"),
+            ul: document.querySelector("#plugin-toc .toc-ul"),
+        };
+
+        this.onResize();
+        this.utils.addEventListener(this.utils.eventType.outlineUpdated, () => this.entities.modal.style.display !== "none" && this.renewModal());
+        this.entities.modal.addEventListener("click", ev => {
             const target = ev.target.closest(".toc-node");
             if (!target) return;
             ev.stopPropagation();
@@ -16,35 +28,63 @@ class tocPlugin extends BaseCustomPlugin {
         })
     }
 
-    callback = () => this.toggle(this.toc.style.display === "none")
+    callback = () => this.toggle(this.entities.modal.style.display !== "none")
 
     toggle = (show = false) => {
-        const content = document.querySelector("content");
-        if (!show) {
-            this.toc.style.display = "none";
-            content.style.removeProperty("right");
-            content.style.removeProperty("width");
+        if (show) {
+            this.entities.modal.style.display = "none";
+            this.entities.content.style.removeProperty("right");
+            this.entities.content.style.removeProperty("width");
             return
         }
 
-        const {top, width, height, right} = content.getBoundingClientRect();
-        this.toc.style.display = "block";
+        this.entities.modal.style.removeProperty("display");
+        const {width, right} = this.entities.content.getBoundingClientRect();
         this.renewModal();
-        const modalWidth = this.toc.getBoundingClientRect().width;
-        const tocStyle = {top: `${top}px`, height: `${height}px`};
+        const modalWidth = width * this.config.width_percent_when_pin_right / 100;
         const contentStyle = {right: `${right - modalWidth}px`, width: `${width - modalWidth}px`};
-        Object.assign(this.toc.style, tocStyle);
-        Object.assign(content.style, contentStyle);
+        this.entities.modal.style.width = modalWidth + "px";
+        Object.assign(this.entities.content.style, contentStyle);
     }
 
     renewModal = () => {
-        const ul = this.getTocTemplate();
+        const ul = this._getTocTemplate();
         const toc = this.utils.createElement(ul);
-        this.toc.firstElementChild && this.toc.removeChild(this.toc.firstElementChild);
-        this.toc.appendChild(toc);
+        this.entities.ul.firstElementChild && this.entities.ul.removeChild(this.entities.ul.firstElementChild);
+        this.entities.ul.appendChild(toc);
     }
 
-    getTocTemplate = () => {
+    onResize = () => {
+        let contentStartRight = 0;
+        let contentStartWidth = 0;
+        let modalStartLeft = 0;
+        let contentMaxRight = 0;
+        const onMouseDown = () => {
+            const contentRect = this.entities.content.getBoundingClientRect();
+            contentStartRight = contentRect.right;
+            contentStartWidth = contentRect.width;
+
+            const modalRect = this.entities.modal.getBoundingClientRect();
+            modalStartLeft = modalRect.left;
+            contentMaxRight = modalRect.right - 50;
+        }
+        const onMouseMove = (deltaX, deltaY) => {
+            deltaX = -deltaX;
+            deltaY = -deltaY;
+            let newContentRight = contentStartRight - deltaX;
+            if (newContentRight > contentMaxRight) {
+                newContentRight = contentMaxRight;
+                deltaX = contentStartRight - contentMaxRight;
+            }
+            this.entities.content.style.right = newContentRight + "px";
+            this.entities.content.style.width = contentStartWidth - deltaX + "px";
+            this.entities.modal.style.left = modalStartLeft - deltaX + "px";
+            return {deltaX, deltaY}
+        }
+        this.utils.resizeFixedModal(this.entities.grip, this.entities.modal, true, false, onMouseDown, onMouseMove);
+    }
+
+    _getTocTemplate = () => {
         const rootNode = this._getTocRootNode();
         const li = rootNode.children.map(this._tocTemplate);
         return {ele: "ul", children: li}
