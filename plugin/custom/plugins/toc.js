5498d967a2921c4496ed93378a75871f85a60aeb
obgnail <he1251698542@gmail.com>
2024-02-02 17:38:38 +0800

    update toc

--- plugin/custom/plugins/toc.js
+++ plugin/custom/plugins/toc.js
@@ -5,9 +5,7 @@ class tocPlugin extends BaseCustomPlugin {
 
     process = () => {
         this.toc = document.querySelector("#plugin-toc");
-
         this.utils.addEventListener(this.utils.eventType.outlineUpdated, () => this.toc.style.display === "block" && this.renewModal());
-
         this.toc.addEventListener("click", ev => {
             const target = ev.target.closest(".toc-node");
             if (!target) return;
@@ -33,7 +31,7 @@ class tocPlugin extends BaseCustomPlugin {
         this.toc.style.display = "block";
         this.renewModal();
         const modalWidth = this.toc.getBoundingClientRect().width;
-        const tocStyle = {top: `${top}px`, height: `${height}px`, left: `${right - modalWidth}px`};
+        const tocStyle = {top: `${top}px`, height: `${height}px`};
         const contentStyle = {right: `${right - modalWidth}px`, width: `${width - modalWidth}px`};
         Object.assign(this.toc.style, tocStyle);
         Object.assign(content.style, contentStyle);
@@ -48,44 +46,38 @@ class tocPlugin extends BaseCustomPlugin {
 
     getTocTemplate = () => {
         const rootNode = this._getTocRootNode();
-        const li = this._tocTemplate(rootNode);
-        return {ele: "ul", children: li.children}
+        const li = rootNode.children.map(this._tocTemplate);
+        return {ele: "ul", children: li}
     }
 
     _getTocRootNode = () => {
-        const _newTocNode = (depth, cid, text) => ({depth, cid, text, children: []})
-        const root = _newTocNode(0, "n0", "root");
-        const {headers} = File.editor.nodeMap.toc || {};
+        const root = {depth: 0, cid: "n0", text: "root", children: []};
+        const {headers = []} = File.editor.nodeMap.toc;
         if (!headers) return root;
 
-        const arr = [root, null, null, null, null, null, null];
-        for (const {attributes: {depth, text}, cid} of headers) {
-            const node = _newTocNode(depth, cid, text)
-            const parent = this._findLast(arr, depth);
+        const toc = headers.map(({attributes: {depth, text}, cid}) => ({depth, text, cid, children: []}));
+        toc.forEach((node, idx) => {
+            const parent = this._findParent(toc, idx - 1, node.depth) || root;
             parent.children.push(node);
-            arr[depth] = node;
-        }
+        })
         return root
     }
 
     _tocTemplate = rootNode => {
-        const {text, cid, children = []} = rootNode;
+        const {text, cid, depth, children = []} = rootNode;
         const content = [{class_: "toc-node", cid, children: [{ele: "span", class_: "toc-text", text}]}];
         const list = children.map(this._tocTemplate);
         if (list.length) {
             content.push({ele: "ul", children: list});
         }
-        return {ele: "li", children: content}
+        return {ele: "li", class_: "plugin-toc-depth" + depth, children: content}
     }
 
-    _findLast = (arr, stop) => {
-        let diff = 1;
-        let target = arr[stop - diff];
-        while (!target) {
-            diff++;
-            target = arr[stop - diff];
+    _findParent = (toc, idx, depth) => {
+        while (idx >= 0 && toc[idx].depth >= depth) {
+            idx--;
         }
-        return target
+        return toc[idx]
     }
 }
 
