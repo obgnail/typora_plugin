08f1b7edbba4637cac80d04da4f670e64e6d080c
obgnail <he1251698542@gmail.com>
2024-02-23 16:14:53 +0800

    update markmap

--- plugin/markmap/index.js
+++ plugin/markmap/index.js
@@ -207,7 +207,7 @@ class tocMarkmap {
                     <div class="plugin-markmap-icon ion-arrow-expand" action="expand" ty-hint="全屏"></div>
                     <div class="plugin-markmap-icon ion-arrow-move" action="move" ty-hint="移动（ctrl+drag也可以移动）"></div>
                     <div class="plugin-markmap-icon ion-cube" action="fit" ty-hint="图表重新适配窗口"></div>
-                    <div class="plugin-markmap-icon ion-android-hand" action="setPointerEvent" ty-hint="图表是否响应鼠标事件"></div>
+                    <div class="plugin-markmap-icon ion-android-hand" action="setPointerEvent" ty-hint="鼠标穿透"></div>
                     <div class="plugin-markmap-icon ion-archive" action="download" ty-hint="下载"></div>
                     <div class="plugin-markmap-icon ion-chevron-up" action="pinUp" ty-hint="固定到顶部"></div>
                     <div class="plugin-markmap-icon ion-chevron-right" action="pinRight" ty-hint="固定到右侧"></div>
@@ -289,6 +289,7 @@ class tocMarkmap {
         this.markmap.destroy();
         const md = this.controller.getToc();
         await this.create(md, options);
+        this.entities.modal.style.pointerEvents = (options.zoom || options.pan) ? "auto" : "none";
     }
 
     getSvgBounding = cloneSvg => {
