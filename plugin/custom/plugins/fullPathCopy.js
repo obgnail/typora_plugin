0d86939dadb3289b662fd108a3e643d96652fcc9
obgnail <he1251698542@gmail.com>
2024-03-21 12:12:08 +0800

    update doc

--- plugin/custom/plugins/fullPathCopy.js
+++ plugin/custom/plugins/fullPathCopy.js
@@ -3,6 +3,7 @@ class fullPathCopy extends BaseCustomPlugin {
     hint = () => "将当前标题的路径复制到剪切板"
     hotkey = () => [this.config.hotkey]
     callback = anchorNode => {
+        const getHeaderName = (title, name) => title + (this.config.add_space ? " " : "") + name
         const paragraphList = ["H1", "H2", "H3", "H4", "H5", "H6"];
         const nameList = ["一级标题", "二级标题", "三级标题", "四级标题", "五级标题", "六级标题"];
         const pList = [];
@@ -21,21 +22,21 @@ class fullPathCopy extends BaseCustomPlugin {
 
         pList.reverse();
 
-        let filePath = (this.config.full_file_path) ? this.utils.getFilePath() : File.getFileName();
+        let filePath = this.config.full_file_path ? this.utils.getFilePath() : File.getFileName();
         filePath = filePath || this.config.untitled_file_name;
         const result = [filePath];
         let headerIdx = 0;
         for (const p of pList) {
             while (headerIdx < 6 && p.ele.tagName !== paragraphList[headerIdx]) {
                 if (!this.config.ignore_empty_header) {
-                    const name = this.getHeaderName("无", nameList[headerIdx]);
+                    const name = getHeaderName("无", nameList[headerIdx]);
                     result.push(name);
                 }
                 headerIdx++;
             }
 
             if (p.ele.tagName === paragraphList[headerIdx]) {
-                const name = this.getHeaderName(p.ele.querySelector("span").textContent, nameList[headerIdx]);
+                const name = getHeaderName(p.ele.querySelector("span").textContent, nameList[headerIdx]);
                 result.push(name);
                 headerIdx++;
             }
@@ -43,11 +44,6 @@ class fullPathCopy extends BaseCustomPlugin {
 
         const text = this.utils.Package.Path.join(...result);
         navigator.clipboard.writeText(text);
-    }
-
-    getHeaderName = (title, name) => {
-        const space = (this.config.add_space) ? " " : "";
-        return title + space + name
     }
 }
 
