0dd79732da7c8524f606edb4d4abae91d122804e
obgnail <he1251698542@gmail.com>
2024-05-06 12:24:21 +0800

    update markmap

--- plugin/markmap/index.js
+++ plugin/markmap/index.js
@@ -205,7 +205,11 @@ class tocMarkmap {
                     <div class="plugin-markmap-icon ion-chevron-right" action="pinRight" ty-hint="固定到右侧"></div>
                 </div>
                 <svg id="plugin-markmap-svg"></svg>
-                <div class="plugin-markmap-icon ion-android-arrow-down-right" action="resize" ty-hint="拖动调整大小"></div>
+                <div class="plugin-markmap-icon" action="resize" ty-hint="拖动调整大小">
+                    <svg width="800px" height="800px" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M14.228 16.227a1 1 0 0 1-.707-1.707l1-1a1 1 0 0 1 1.416 1.414l-1 1a1 1 0 0 1-.707.293zm-5.638 0a1 1 0 0 1-.707-1.707l6.638-6.638a1 1 0 0 1 1.416 1.414l-6.638 6.638a1 1 0 0 1-.707.293zm-5.84 0a1 1 0 0 1-.707-1.707L14.52 2.043a1 1 0 1 1 1.415 1.414L3.457 15.934a1 1 0 0 1-.707.293z"></path>
+                    </svg>
+                </div>
             </div>
             <div class="plugin-markmap-grip grip-up plugin-common-hidden"></div>
         </div>
