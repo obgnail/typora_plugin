ade968ec8610c0bb8e9853c46d174396718e1717
obgnail <he1251698542@gmail.com>
2024-05-17 17:20:32 +0800

    update markmap

--- plugin/markmap/index.js
+++ plugin/markmap/index.js
@@ -408,16 +408,19 @@ class tocMarkmap {
 
         const ability = () => {
             const {zoom = true, pan = true} = (this.markmap && this.markmap.options) || {};
-            const autoFitLabel = "自动适配窗口" + _genInfo("图形更新时自动重新适配窗口大小");
+            const fitWhenUpdateLabel = "更新时自动适配窗口" + _genInfo("图形更新时自动重新适配窗口大小");
+            const fitWhenFoldLabel = "折叠时自动适配窗口" + _genInfo("折叠图形节点时自动重新适配窗口大小");
             const list = [
                 {label: "鼠标滚轮缩放", value: "zoom", checked: zoom},
                 {label: "鼠标滚轮平移", value: "pan", checked: pan},
-                {label: autoFitLabel, value: "autoFit", checked: this.config.AUTO_FIT_WHEN_UPDATE},
+                {label: fitWhenUpdateLabel, value: "fitWhenUpdate", checked: this.config.AUTO_FIT_WHEN_UPDATE},
+                {label: fitWhenFoldLabel, value: "fitWhenFold", checked: this.config.AUTO_FIT_WHEN_FOLD},
             ];
             const callback = submit => {
                 this.markmap.options.zoom = submit.includes("zoom");
                 this.markmap.options.pan = submit.includes("pan");
-                this.config.AUTO_FIT_WHEN_UPDATE = submit.includes("autoFit");
+                this.config.AUTO_FIT_WHEN_UPDATE = submit.includes("fitWhenUpdate");
+                this.config.AUTO_FIT_WHEN_FOLD = submit.includes("fitWhenFold");
             };
             return {label: "", legend: "能力", type: "checkbox", list, callback}
         }
@@ -820,9 +823,16 @@ class tocMarkmap {
     }
 
     onSvgClick = () => {
-        if (!this.config.CLICK_TO_LOCALE) return;
+        if (!this.config.CLICK_TO_LOCALE || !this.config.AUTO_FIT_WHEN_FOLD) return;
         this.entities.svg.addEventListener("click", ev => {
-            if (ev.target.closest("circle")) return;
+            if (ev.target.closest("circle")) {
+                if (this.config.AUTO_FIT_WHEN_FOLD) {
+                    const timeout = (this.markmap && this.markmap.options.duration) || 500;
+                    setTimeout(this.fit, timeout);
+                }
+                return;
+            }
+
             const target = ev.target.closest(".markmap-node");
             if (!target) return;
             ev.stopPropagation();
