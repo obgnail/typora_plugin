c711a92cb053ba22807b9556f0a0c1dc30a55869
AlbertZhang <han892577@163.com>
2024-05-28 18:22:44 +0800

    update load custom plugin path

--- plugin/pluginPaths.js
+++ plugin/pluginPaths.js
@@ -2,10 +2,10 @@
  * 添加插件名称和所属文件夹，供插件动态加载
  * */
 const PLUGIN_PATHS = {
-    'upload_to_csdn': './plugin/uploadArticle',
-    'upload_to_wordpress': './plugin/uploadArticle',
-    'upload_to_cn_blog': './plugin/uploadArticle',
-    'upload_to_all_platform': './plugin/uploadArticle',
+    'upload_to_csdn': './plugin/uploadArticle/plugins/',
+    'upload_to_wordpress': './plugin/uploadArticle/plugins/',
+    'upload_to_cn_blog': './plugin/uploadArticle/plugins/',
+    'upload_to_all_platform': './plugin/uploadArticle/plugins/',
 };
 
 module.exports = {PLUGIN_PATHS};
