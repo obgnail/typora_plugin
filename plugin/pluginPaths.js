9107b18154c8afa1d9ef6899371a55b094914f6d
AlbertZhang <han892577@163.com>
2024-05-28 22:12:43 +0800

    update dynamic load plugin path config

--- plugin/pluginPaths.js
+++ plugin/pluginPaths.js
@@ -2,10 +2,7 @@
  * 添加插件名称和所属文件夹，供插件动态加载
  * */
 const PLUGIN_PATHS = {
-    'upload_to_csdn': './plugin/uploadArticle/plugins/',
-    'upload_to_wordpress': './plugin/uploadArticle/plugins/',
-    'upload_to_cn_blog': './plugin/uploadArticle/plugins/',
-    'upload_to_all_platform': './plugin/uploadArticle/plugins/',
+    'article_uploader': './plugin/uploadArticle',
 };
 
 module.exports = {PLUGIN_PATHS};
