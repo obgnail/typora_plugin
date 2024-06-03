7c9cf2b203901b59d21b5d4c39ca80a542784a46
obgnail <he1251698542@gmail.com>
2024-06-03 19:03:16 +0800

    update pie_menu

--- plugin/pie_menu.js
+++ plugin/pie_menu.js
@@ -13,47 +13,57 @@ class pieMenu extends BasePlugin {
 
     init = () => {
         this.entities = {
+            content: document.querySelector("content"),
             menu: document.querySelector(".plugin-pie-menu"),
-            label: document.querySelector(".plugin-pie-menu-label"),
+            items: document.querySelector(".plugin-pie-menu-menu"),
+            // label: document.querySelector(".plugin-pie-menu-label"),
         }
     }
 
-    showMenu = ev => {
-        const {clientX, clientY} = ev;
+    showMenu = (x, y) => {
+        if (!x && !y) {
+            x = (window.innerWidth || document.documentElement.clientWidth) / 2;
+            y = (window.innerHeight || document.documentElement.clientHeight) / 2;
+        }
         this.utils.show(this.entities.menu);
         const {width, height} = this.entities.menu.getBoundingClientRect();
-        const position = {left: clientX - width / 2 + "px", top: clientY - height / 2 + "px"};
+        const position = {left: x - width / 2 + "px", top: y - height / 2 + "px"};
         Object.assign(this.entities.menu.style, position);
     }
 
-    hideMenu = () => {
-        this.utils.hide(this.entities.menu);
-    }
-
-    _onClick = ev => {
-        ev.stopPropagation();
-        ev.preventDefault();
-        const target = ev.target.closest(".plugin-pie-menu-menu-item[data-callback]");
-        const callback = target && target.dataset.callback;
-        if (callback) {
-            const [fixedName, callArg] = callback.split(".");
-            this.utils.generateDynamicCallArgs(fixedName);
-            const plugin = this.utils.getPlugin(fixedName);
-            plugin && plugin.call && this.utils.withMeta(meta => plugin.call(callArg, meta));
-        }
-        this.hideMenu();
-        document.removeEventListener("click", this._onClick, true);
-    }
+    isMenuShown = () => this.utils.isShow(this.entities.menu)
+    hideMenu = () => this.utils.hide(this.entities.menu);
+    toggleMenu = () => this.utils.toggleVisible(this.entities.menu)
 
     process = () => {
-        document.querySelector("content").addEventListener("contextmenu", ev => {
-            if (!this.utils.metaKeyPressed(ev)) return;
-            ev.stopPropagation();
-            ev.preventDefault();
-            this.showMenu(ev);
-            document.addEventListener("click", this._onClick, true);
+        this.entities.content.addEventListener("contextmenu", ev => {
+            if (this.utils.metaKeyPressed(ev)) {
+                ev.stopPropagation();
+                ev.preventDefault();
+                this.showMenu(ev.clientX, ev.clientY);
+            }
         }, true)
+
+        this.entities.content.addEventListener("click", ev => {
+            if (this.isMenuShown() && !ev.target.closest(".plugin-pie-menu")) {
+                this.hideMenu();
+            }
+        })
+
+        this.entities.items.addEventListener("click", ev => {
+            const target = ev.target.closest(".plugin-pie-menu-menu-item[data-callback]");
+            const callback = target && target.dataset.callback;
+            if (callback) {
+                const [fixedName, callArg] = callback.split(".");
+                this.utils.generateDynamicCallArgs(fixedName);
+                const plugin = this.utils.getPlugin(fixedName);
+                plugin && plugin.call && this.utils.withMeta(meta => plugin.call(callArg, meta));
+                this.hideMenu();
+            }
+        })
     }
+
+    call = () => this.toggleMenu();
 }
 
 module.exports = {
