925364d950b39689d303bdf890ec447e1c41b43a
obgnail <he1251698542@gmail.com>
2024-07-30 19:05:06 +0800

    update auto_number

--- plugin/auto_number.js
+++ plugin/auto_number.js
@@ -4,8 +4,8 @@ class autoNumberPlugin extends BasePlugin {
 
         this.base_css = `
         #write { counter-reset: write-h2 Figures Tables Fences; }
-        #write > h1 { counter-reset: write-h2 Figures Tables Fences; }
-        #write > h2 { counter-reset: write-h3 Figures Tables Fences; }
+        #write > h1 { counter-reset: write-h2; }
+        #write > h2 { counter-reset: write-h3; }
         #write > h3 { counter-reset: write-h4; }
         #write > h4 { counter-reset: write-h5; }
         #write > h5 { counter-reset: write-h6; }
@@ -138,7 +138,7 @@ class autoNumberPlugin extends BasePlugin {
 
         const image_content = `
             counter-increment: Figures;
-            content: "${this.config.NAMES.image} " counter(write-h2) "-" counter(Figures);
+            content: "${this.config.NAMES.image} " counter(Figures);
             font-family: ${this.config.FONT_FAMILY};
             display: block;
             text-align: ${this.config.ALIGN};
@@ -150,7 +150,7 @@ class autoNumberPlugin extends BasePlugin {
         this.table_css = `
         #write .table-figure::after {
             counter-increment: Tables;
-            content: "${this.config.NAMES.table} " counter(write-h2) "-" counter(Tables);
+            content: "${this.config.NAMES.table} " counter(Tables);
             font-family: ${this.config.FONT_FAMILY};
             display: block;
             text-align: ${this.config.ALIGN};
@@ -163,7 +163,7 @@ class autoNumberPlugin extends BasePlugin {
         }
         #write .md-fences::after {
             counter-increment: Fences;
-            content: "${this.config.NAMES.fence} " counter(write-h2) "-" counter(Fences);
+            content: "${this.config.NAMES.fence} " counter(Fences);
             position: absolute;
             width: 100%;
             text-align: ${this.config.ALIGN};
