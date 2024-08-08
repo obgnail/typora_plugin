80cbcbea5cc64af6ef649fa05bd621467fb5a3c9
obgnail <he1251698542@gmail.com>
2024-08-08 15:40:28 +0800

    update linterWorker

--- plugin/custom/plugins/markdownLint/linterWorker.js
+++ plugin/custom/plugins/markdownLint/linterWorker.js
@@ -1,24 +1,25 @@
 const require = self.require;
-const fs = require("fs");
+const fs = require("fs").promises;
 
-let markdownlinter = null;
-const rules = {
-    default: true,
-    MD024: { siblings_only: true },
-};
+let linter = null;
+const config = { default: true, MD024: { siblings_only: true } };
 
 const init = disabled => {
-    markdownlinter = require("./markdownlint.min").markdownlint;
+    const { markdownlint } = require("./markdownlint.min");
+    linter = markdownlint;
+
     for (const rule of disabled) {
-        rules[rule] = false;
+        config[rule] = false;
     }
-    console.debug(`markdown linter worker is initialized with rules`, rules);
+
+    console.debug(`markdown linter worker is initialized with rules`, config);
 }
 
 const lint = async filepath => {
-    if (!markdownlinter || !rules) return;
-    const fileContent = await fs.promises.readFile(filepath, 'utf-8');
-    const { content } = markdownlinter.sync({ strings: { content: fileContent }, config: rules });
+    if (!linter) return;
+
+    const fileContent = await fs.readFile(filepath, "utf-8");
+    const { content } = linter.sync({ strings: { content: fileContent }, config });
     content.sort((a, b) => a.lineNumber - b.lineNumber);
     return content
 }
