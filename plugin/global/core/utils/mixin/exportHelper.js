f10626ab046a6e08dbcf026019c1c282d3891316
obgnail <he1251698542@gmail.com>
2025-02-10 20:19:39 +0800

    feat: core/exportHelper: supports exportToNative

--- plugin/global/core/utils/mixin/exportHelper.js
+++ plugin/global/core/utils/mixin/exportHelper.js
@@ -3,85 +3,110 @@
  */
 class exportHelper {
     constructor(utils) {
-        this.utils = utils;
-        this.helpers = new Map();
-        this.isAsync = undefined;
+        this.utils = utils
+        this.htmlHelpers = new Map()
+        this.nativeHelpers = new Map()
+        this.isAsync = undefined
     }
 
     /**
      * @param {string} name: 取个名字
-     * @param {function(exportOptions): string | null} beforeExport: 如果返回string，将加入到extraCSS
-     * @param {function(html, exportOptions): html | null} afterExport: 如果返回string，将替换HTML
+     * @param {function(exportOptions): string | null} beforeExportToHTML: 如果返回string，将加入到extraCSS
+     * @param {function(html, exportOptions): html | null} afterExportToHTML: 如果返回string，将替换HTML
      */
-    register = (name, beforeExport, afterExport) => this.helpers.set(name, { beforeExport, afterExport });
-    unregister = name => this.helpers.delete(name);
+    register = (name, beforeExportToHTML, afterExportToHTML) => this.htmlHelpers.set(name, { beforeExportToHTML, afterExportToHTML })
+    unregister = name => this.htmlHelpers.delete(name)
 
-    beforeExport = (...args) => {
-        if (!this.utils.isObject(args[0])) return;
-
-        for (const h of this.helpers.values()) {
-            if (!h.beforeExport) continue;
-            try {
-                const css = h.beforeExport(...args);
-                if (css && typeof css === "string") {
-                    args[0].extraCss = (args[0].extraCss || "") + css;
-                }
-            } catch (e) {
-                console.error("BeforeExport Error:", e);
-            }
-        }
-    }
+    /** 使用 pandoc 导出 */
+    registerNative = (name, beforeExportToNative, afterExportToNative) => this.nativeHelpers.set(name, { beforeExportToNative, afterExportToNative })
+    unregisterNative = name => this.nativeHelpers.delete(name)
 
     check = args => {
-        const { type } = args[0] || {};
+        const { type } = args[0] || {}
         return type === "html" || type === "html-plain" || type === "pdf" || type === "image"
     }
 
-    afterExport = async (exportResult, ...args) => {
-        if (!this.check(args)) return exportResult
+    processExportHTML = () => {
+        const beforeFn = (...args) => {
+            if (!this.utils.isObject(args[0])) return
 
-        let html = await exportResult;
-        for (const h of this.helpers.values()) {
-            if (!h.afterExport) continue;
-            try {
-                const newHtml = await h.afterExport(html, ...args);
-                if (newHtml && typeof newHtml === "string") {
-                    html = newHtml;
+            for (const h of this.htmlHelpers.values()) {
+                if (!h.beforeExportToHTML) continue
+                try {
+                    const css = h.beforeExportToHTML(...args)
+                    if (css && typeof css === "string") {
+                        args[0].extraCss = (args[0].extraCss || "") + css
+                    }
+                } catch (e) {
+                    console.error("beforeExportToHTML Error:", e)
                 }
-            } catch (e) {
-                console.error("AfterExport Error:", e);
             }
         }
-        return html;
+        const afterFn = async (exportResult, ...args) => {
+            if (!this.check(args)) return exportResult
+
+            let html = await exportResult
+            for (const h of this.htmlHelpers.values()) {
+                if (!h.afterExportToHTML) continue
+                try {
+                    const newHtml = await h.afterExportToHTML(html, ...args)
+                    if (newHtml && typeof newHtml === "string") {
+                        html = newHtml
+                    }
+                } catch (e) {
+                    console.error("afterExportToHTML Error:", e)
+                }
+            }
+            return html
+        }
+        const afterFnSync = (exportResult, ...args) => {
+            if (!this.check(args)) return exportResult
+
+            let html = exportResult
+            for (const h of this.htmlHelpers.values()) {
+                if (!h.afterExportToHTML) continue
+                try {
+                    const newHtml = h.afterExportToHTML(html, ...args)
+                    if (newHtml && !this.utils.isPromise(newHtml)) {
+                        html = newHtml
+                    }
+                } catch (e) {
+                    console.error("afterExportToHTMLSync Error:", e)
+                }
+            }
+            return html
+        }
+
+        // 旧版本的Typora的export函数不是AsyncFunction，尽最大努力兼容旧版本
+        const callback = () => {
+            this.isAsync = this.utils.isAsyncFunction(File.editor.export.exportToHTML)
+            const after = this.isAsync ? afterFn : afterFnSync
+            this.utils.decorate(() => File.editor.export, "exportToHTML", beforeFn, after, true)
+        }
+        this.utils.loopDetector(() => File && File.editor && File.editor.export && File.editor.export.exportToHTML, callback)
     }
 
-    afterExportSync = (exportResult, ...args) => {
-        if (!this.check(args)) return exportResult
-
-        let html = exportResult;
-        for (const h of this.helpers.values()) {
-            if (!h.afterExport) continue;
-            try {
-                const newHtml = h.afterExport(html, ...args);
-                if (newHtml && !this.utils.isPromise(newHtml)) {
-                    html = newHtml;
+    processExportToNative = () => {
+        const getLifeCycleFn = fnName => (...args) => {
+            for (const h of this.nativeHelpers.values()) {
+                const fn = h[fnName]
+                if (!fn) continue
+                try {
+                    fn(...args)
+                } catch (e) {
+                    console.error(`${fnName} Error:`, e)
                 }
-            } catch (e) {
-                console.error("afterExportSync Error:", e);
             }
         }
-        return html
+        const beforeFn = getLifeCycleFn("beforeExportToNative")
+        const afterFn = getLifeCycleFn("afterExportToNative")
+        const callback = () => this.utils.decorate(() => File.editor.export, "exportToNative", beforeFn, afterFn)
+        this.utils.loopDetector(() => File && File.editor && File.editor.export && File.editor.export.exportToNative, callback)
     }
 
     process = () => {
-        // 旧版本的Typora的export函数不是AsyncFunction，尽最大努力兼容旧版本
-        const until = () => File && File.editor && File.editor.export && File.editor.export.exportToHTML
-        const callback = () => {
-            this.isAsync = this.utils.isAsyncFunction(File.editor.export.exportToHTML);
-            const after = this.isAsync ? this.afterExport : this.afterExportSync
-            this.utils.decorate(() => File && File.editor && File.editor.export, "exportToHTML", this.beforeExport, after, true)
-        }
-        this.utils.loopDetector(until, callback);
+        this.processExportHTML()
+        this.processExportToNative()
     }
 }
 
