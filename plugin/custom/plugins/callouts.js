efc6147199c7aed7b34b635d7f580c9ae5232ca9
obgnail <he1251698542@gmail.com>
2025-04-25 22:28:22 +0800

    feat: callouts: add config option use_network_icon_when_exporting、network_icon_url

--- plugin/custom/plugins/callouts.js
+++ plugin/custom/plugins/callouts.js
@@ -52,9 +52,19 @@ class calloutsPlugin extends BaseCustomPlugin {
     }
 
     beforeExport = (...args) => {
-        if (this.check(args)) {
-            return this.utils.styleTemplater.getStyleContent(this.fixedName);
-        }
+        if (!this.check(args)) return
+
+        const extra = `
+            @font-face {
+                font-family: '${this.config.font_family}';
+                src: url(${this.config.network_icon_url}) format('woff');
+                font-weight: normal;
+                font-style: normal;
+            }`
+        const css = this.utils.styleTemplater.getStyleContent(this.fixedName)
+        return this.config.use_network_icon_when_exporting
+            ? extra + css
+            : css.replace(/--callout-icon: ".*?";/g, "")
     }
 
     afterExport = (html, ...args) => {
