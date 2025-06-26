c4a1ee634b38f378837bb04b10ea18fb9231928f
obgnail <he1251698542@gmail.com>
2025-06-26 22:54:19 +0800

    update utils/createConsecutiveAction

--- plugin/global/core/utils/index.js
+++ plugin/global/core/utils/index.js
@@ -243,8 +243,11 @@ class utils {
         {
             threshold = 2,
             timeWindow = 1000,
+            totalTimeLimit = 0,
             resetOnConfirmed = true,
             getIdentifier = (...args) => undefined,
+            shouldReset = () => false,
+            shouldConfirm = () => false,
             onTimeout = this.noop,
             onInsufficient = (current, total) => this.notification.show(i18n.t("global", "confirmNeeded", { count: total - current }), "info"),
             onConfirmed,
@@ -254,19 +257,54 @@ class utils {
             throw new Error("onConfirmed must be a Function")
         }
         threshold = Math.max(threshold, 2)
+        totalTimeLimit = Math.max(totalTimeLimit, 0)
 
         const NO_IDENTIFIER = Symbol("no_identifier")
         let currentCount = 0
         let lastTimestamp = 0
-        let resetTimer = null
+        let firstTimestamp = 0
         let lastIdentifier = NO_IDENTIFIER
+        let resetTimer = null
+
+        const resetState = () => {
+            currentCount = 0
+            lastTimestamp = 0
+            firstTimestamp = 0
+            lastIdentifier = NO_IDENTIFIER
+            if (resetTimer) {
+                clearTimeout(resetTimer)
+                resetTimer = null
+            }
+        }
+        const executeConfirmation = (...args) => {
+            onConfirmed(...args)
+            if (resetOnConfirmed) {
+                resetState()
+            }
+        }
 
         return function (...args) {
+            if (shouldConfirm(...args)) {
+                executeConfirmation(...args)
+                return
+            }
+
             const now = Date.now()
             const currentIdentifier = getIdentifier(...args)
+            if (shouldReset(...args)) {
+                resetState()
+            }
 
-            if (currentCount === 0 || now - lastTimestamp > timeWindow || currentIdentifier !== lastIdentifier) {
+            const needReset = (
+                currentCount === 0
+                || now - lastTimestamp > timeWindow
+                || (totalTimeLimit > 0 && currentCount > 0 && now - firstTimestamp > totalTimeLimit)
+                || currentIdentifier !== lastIdentifier
+            )
+            if (needReset) {
+                resetState()
                 currentCount = 1
+                firstTimestamp = now
             } else {
                 currentCount++
             }
@@ -279,20 +317,14 @@ class utils {
 
             if (currentCount < threshold) {
                 resetTimer = setTimeout(() => {
-                    if (onTimeout && currentCount < threshold) {
+                    if (currentCount > 0 && currentCount < threshold) {
                         onTimeout(currentCount, threshold)
                     }
-                    currentCount = 0
-                    lastIdentifier = NO_IDENTIFIER
-                    resetTimer = null
+                    resetState()
                 }, timeWindow)
                 onInsufficient(currentCount, threshold)
             } else {
-                onConfirmed(...args)
-                if (resetOnConfirmed) {
-                    currentCount = 0
-                    lastIdentifier = NO_IDENTIFIER
-                }
+                executeConfirmation(...args)
             }
         }
     }
