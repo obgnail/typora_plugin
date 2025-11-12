55009628c6d194e540d99a6fddc098bd3c0186c3
obgnail <he1251698542@gmail.com>
2025-11-12 20:58:22 +0800

    fix(go_top): Not available in typewriter mode (#729)

--- plugin/go_top.js
+++ plugin/go_top.js
@@ -1,4 +1,4 @@
-class goTopPlugin extends BasePlugin {
+class GoTopPlugin extends BasePlugin {
     hotkey = () => [
         { hotkey: this.config.HOTKEY_GO_TOP, callback: this.goTop },
         { hotkey: this.config.HOTKEY_GO_BOTTOM, callback: this.goBottom },
@@ -7,6 +7,10 @@ class goTopPlugin extends BasePlugin {
     call = action => {
         const func = (action === "go-bottom") ? "jumpBottom" : "jumpTop"
         File.editor.selection[func]()
+        if (File.isTypeWriterMode) {
+            const scrollTop = (action === "go-bottom") ? this.utils.entities.eWrite.getBoundingClientRect().height : "0"
+            this.utils.entities.$eContent.animate({ scrollTop }, "100", "swing", () => File.editor.library.outline.highlightVisibleHeader())
+        }
     }
 
     goTop = () => this.call("go-top")
@@ -14,5 +18,5 @@ class goTopPlugin extends BasePlugin {
 }
 
 module.exports = {
-    plugin: goTopPlugin,
+    plugin: GoTopPlugin
 }
